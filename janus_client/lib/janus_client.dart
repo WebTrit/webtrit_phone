f117498d2650b7b414173bfcde4138e8e9202ab3
Yurii Cherniavskyi <ych@webtrit.com>
2020-04-01 14:30:34 +0300

    janus_client: Remove boilerplate class and its test

--- janus_client/lib/janus_client.dart
+++ janus_client/lib/janus_client.dart
@@ -1,7 +1,2 @@
 library janus_client;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
--- janus_client/test/janus_client_test.dart
+++ janus_client/test/janus_client_test.dart
@@ -3,11 +3,7 @@ import 'package:test/test.dart';
 import 'package:janus_client/janus_client.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('temporary happy test', () {
+    expect(true, isTrue);
   });
 }
