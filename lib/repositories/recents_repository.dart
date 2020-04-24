2499ff27faaba4b99bd573503acc9f3e1e4d28dc
Yurii Cherniavskyi <ych@webtrit.com>
2020-04-24 11:41:05 +0300

    Remove temporary functionality with with dummy init data from Recents repository

--- lib/repositories/recents_repository.dart
+++ lib/repositories/recents_repository.dart
@@ -4,33 +4,16 @@ import 'package:meta/meta.dart';
 
 import 'package:webtrit_phone/models/models.dart';
 
-// TODO: temporary code
+// TODO: add local persist and/or external sync of recents calls
 class RecentsRepository {
   final List<Recent> _recents = [];
   final StreamController _controller = StreamController<List<Recent>>.broadcast();
-
-  int _fetchCallCounter = 0;
 
   Stream<List<Recent>> recents() {
     return _controller.stream;
   }
 
   Future<void> load() async {
-    if (_recents.isEmpty) {
-      _recents.addAll(List.generate(
-        50,
-        (index) => Recent(
-          index % 2 == 0 ? Direction.incoming : Direction.outgoing,
-          index % 3 == 0,
-          'Recent call #$index',
-          DateTime.now().subtract(Duration(hours: index)),
-        ),
-      ));
-    }
-
-    await Future.delayed(Duration(seconds: 1));
-    if (_fetchCallCounter++ % 3 == 0) throw Exception();
-
     _controller.add(List<Recent>.from(_recents));
   }
 
