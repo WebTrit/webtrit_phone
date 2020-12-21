c07a10867dd2a4ec022544802997431c9e8b9789
Yurii Cherniavskyi <ych@webtrit.com>
2020-12-22 00:49:04 +0200

    Add RecentsVisibilityFilter enum module with l10n extension

---
+++ lib/models/recents_visibility_filter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+enum RecentsVisibilityFilter {
+  all,
+  missed,
+  incoming,
+  outgoing,
+}
+
+extension RecentsVisibilityFilterI10nExtension on RecentsVisibilityFilter {
+  String l10n(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context);
+    switch (this) {
+      case RecentsVisibilityFilter.all:
+        return appLocalizations.recentsVisibilityFilterAll;
+      case RecentsVisibilityFilter.missed:
+        return appLocalizations.recentsVisibilityFilterMissed;
+      case RecentsVisibilityFilter.incoming:
+        return appLocalizations.recentsVisibilityFilterIncoming;
+      case RecentsVisibilityFilter.outgoing:
+        return appLocalizations.recentsVisibilityFilterOutgoing;
+      default:
+        return null;
+    }
+  }
+}
