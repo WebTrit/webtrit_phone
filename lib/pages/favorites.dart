077f27d43d02f917daa35db498d5b610ae419fe3
Yurii Cherniavskyi <ych@webtrit.com>
2021-01-21 00:00:43 +0200

    Improve favorites page stub

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -7,12 +7,34 @@ class FavoritesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
     return Scaffold(
       appBar: MainAppBar(),
       body: Center(
-        child: Icon(
-          Icons.star_outline,
-          size: 120,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.star_outline,
+              size: 80,
+              color: themeData.textTheme.caption.color,
+            ),
+            SizedBox(height: 10),
+            Text(
+              'There is no any favorite numbers',
+              style: themeData.textTheme.subtitle1,
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              child: Text('Add new favorite'.toUpperCase()),
+              onPressed: () {
+                Scaffold.of(context).removeCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text('Sorry, not implemented yet'),
+                ));
+              },
+            )
+          ],
         ),
       ),
     );
