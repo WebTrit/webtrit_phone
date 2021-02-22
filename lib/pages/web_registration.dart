325efef4fae744dba2e182aa1a1abb0401a8205e
Yurii Cherniavskyi <ych@webtrit.com>
2021-02-22 15:42:58 +0200

    Tune web registration demo page viewport config

--- lib/pages/web_registration.dart
+++ lib/pages/web_registration.dart
@@ -12,7 +12,7 @@ const String kDemoPage = '''
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>WebTrit Web Registration Example</title>
   <style>
     body {
