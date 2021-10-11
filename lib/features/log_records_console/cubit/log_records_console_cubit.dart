9918d00135f2a8cd58f14f2fce8b57c8f0422d20
Yurii Cherniavskyi <ych@webtrit.com>
2021-10-11 14:28:51 +0300

    Add app name to share subject in Log Records Console feature

--- lib/features/log_records_console/cubit/log_records_console_cubit.dart
+++ lib/features/log_records_console/cubit/log_records_console_cubit.dart
@@ -5,6 +5,7 @@ import 'package:logging_appenders/logging_appenders.dart';
 import 'package:meta/meta.dart';
 import 'package:share_plus/share_plus.dart';
 
+import 'package:webtrit_phone/data/data.dart';
 import 'package:webtrit_phone/repositories/repositories.dart';
 
 part 'log_records_console_state.dart';
@@ -41,6 +42,6 @@ class LogRecordsConsoleCubit extends Cubit<LogRecordsConsoleState> {
       sb.writeln();
     }
 
-    await Share.share(sb.toString(), subject: 'Log Records ${logRecords[0].time}');
+    await Share.share(sb.toString(), subject: '${PackageInfo().appName} Log Records ${logRecords[0].time}');
   }
 }
