1be9acd23d0956a17a8ddc8a088d36fac05d8e09
Yurii Cherniavskyi <ych@webtrit.com>
2021-10-20 12:36:59 +0300

    Implement Account Info repository

---
+++ lib/repositories/account_info/account_info_repository.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:webtrit_api/webtrit_api.dart';
+
+import 'package:webtrit_phone/data/secure_storage.dart';
+
+class AccountInfoRepository {
+  AccountInfoRepository({
+    required this.webtritApiClient,
+    this.periodicPolling = true,
+  }) {
+    _controller = StreamController<AccountInfo>.broadcast(
+      onListen: _onListenCallback,
+      onCancel: _onCancelCallback,
+    );
+    _listenedCounter = 0;
+  }
+
+  final WebtritApiClient webtritApiClient;
+  final bool periodicPolling;
+
+  late StreamController<AccountInfo> _controller;
+  late int _listenedCounter;
+  Timer? _periodicTimer;
+
+  AccountInfo? _info;
+
+  Stream<AccountInfo> info() {
+    return _controller.stream;
+  }
+
+  Future<void> load() async {
+    _info = await _listInfo();
+    _controller.add(_info!);
+  }
+
+  void _onListenCallback() {
+    if (periodicPolling && _listenedCounter++ == 0) {
+      _periodicTimer = Timer.periodic(Duration(seconds: 3), (timer) async {
+        final newInfo = await _listInfo();
+        if (newInfo != _info) {
+          _info = newInfo;
+          _controller.add(_info!);
+        }
+      });
+    }
+  }
+
+  void _onCancelCallback() {
+    if (periodicPolling && --_listenedCounter == 0) {
+      _periodicTimer?.cancel();
+      _periodicTimer = null;
+    }
+  }
+
+  Future<AccountInfo> _listInfo() async {
+    final token = await SecureStorage().readToken();
+    return await webtritApiClient.accountInfo(token!);
+  }
+}
