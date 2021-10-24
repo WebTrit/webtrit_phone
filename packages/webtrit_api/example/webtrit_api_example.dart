0aa56a412e435e13d3ac66c62ff16e856827026a
Yurii Cherniavskyi <ych@webtrit.com>
2021-10-24 17:05:01 +0300

    Implement basic example in webtrit api package

--- packages/webtrit_api/example/webtrit_api_example.dart
+++ packages/webtrit_api/example/webtrit_api_example.dart
@@ -1,5 +1,21 @@
+import 'dart:io';
+
 import 'package:webtrit_api/webtrit_api.dart';
 
-void main() {
-  print('main');
+void main(List<String> arguments) async {
+  if (arguments.length != 2) {
+    stderr.writeln('Incorrect arguments count, must be: <url> <token>');
+    exit(1);
+  }
+
+  final url = arguments[0];
+  final token = arguments[1];
+
+  final client = WebtritApiClient(Uri.parse(url));
+
+  final info = await client.accountInfo(token);
+
+  print(info.toJson());
+
+  client.close();
 }
