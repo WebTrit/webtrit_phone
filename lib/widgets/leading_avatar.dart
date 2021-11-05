6116d0f2b6446f1ff20f45d3fc863a7733b7567e
Yurii Cherniavskyi <ych@webtrit.com>
2021-11-05 15:26:42 +0200

    Add radius properties to leading avatae widget

--- lib/widgets/leading_avatar.dart
+++ lib/widgets/leading_avatar.dart
@@ -4,13 +4,23 @@ class LeadingAvatar extends StatelessWidget {
   const LeadingAvatar({
     Key? key,
     required this.username,
+    this.radius,
+    this.minRadius,
+    this.maxRadius,
   }) : super(key: key);
 
   final String username;
 
+  final double? radius;
+  final double? minRadius;
+  final double? maxRadius;
+
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
+      radius: radius,
+      minRadius: minRadius,
+      maxRadius: maxRadius,
       backgroundColor: Colors.blue.shade100,
       child: Text(username.trim().split(' ').map((v) => v[0]).join()),
     );
