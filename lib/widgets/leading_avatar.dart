1a48eedfe464253a73c0e13cf5b8e25771651915
Yurii Cherniavskyi <ych@webtrit.com>
2021-11-09 23:37:11 +0200

    Fix possible indexing exception in case of empty string in leading avatar widget

--- lib/widgets/leading_avatar.dart
+++ lib/widgets/leading_avatar.dart
@@ -17,12 +17,13 @@ class LeadingAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final username = this.username.trim();
     return CircleAvatar(
       radius: radius,
       minRadius: minRadius,
       maxRadius: maxRadius,
       backgroundColor: Colors.blue.shade100,
-      child: Text(username.trim().split(' ').map((v) => v[0]).join()),
+      child: Text(username.isEmpty ? '' : username.split(' ').map((v) => v[0]).join()),
     );
   }
 }
