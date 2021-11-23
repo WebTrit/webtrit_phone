cf0101ea56fec9f57b6c6dc2e4dd451ab9778fa4
Yurii Cherniavskyi <ych@webtrit.com>
2021-11-23 11:01:45 +0200

    Add unfocuser util
    Links:
    - https://github.com/flutter/flutter/issues/32433
    - https://github.com/caseyryan/flutter_multi_formatter/blob/master/lib/utils/unfocuser.dart

---
+++ lib/utils/unfocuser.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Unfocuser extends StatefulWidget {
+  const Unfocuser({
+    Key? key,
+    this.child,
+    this.minScrollDistanceToIgnore = 10.0,
+  }) : super(key: key);
+
+  final Widget? child;
+  final double minScrollDistanceToIgnore;
+
+  @override
+  _UnfocuserState createState() => _UnfocuserState();
+}
+
+class _UnfocuserState extends State<Unfocuser> {
+  Offset? _touchStartPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (e) {
+        _touchStartPosition = e.position;
+      },
+      onPointerUp: (e) {
+        var touchStopPosition = e.position;
+        if (widget.minScrollDistanceToIgnore > 0.0 && _touchStartPosition != null) {
+          var difference = _touchStartPosition! - touchStopPosition;
+          _touchStartPosition = null;
+          if (difference.distance > widget.minScrollDistanceToIgnore) {
+            return;
+          }
+        }
+
+        var rb = context.findRenderObject() as RenderBox;
+        var result = BoxHitTestResult();
+        rb.hitTest(result, position: touchStopPosition);
+
+        if (result.path.any((entry) => entry.target.runtimeType == IgnoreUnfocuserRenderBox)) {
+          return;
+        }
+
+        var currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: widget.child,
+    );
+  }
+}
+
+class IgnoreUnfocuser extends SingleChildRenderObjectWidget {
+  final Widget child;
+
+  IgnoreUnfocuser({required this.child}) : super(child: child);
+
+  @override
+  IgnoreUnfocuserRenderBox createRenderObject(BuildContext context) {
+    return IgnoreUnfocuserRenderBox();
+  }
+}
+
+class IgnoreUnfocuserRenderBox extends RenderPointerListener {}
