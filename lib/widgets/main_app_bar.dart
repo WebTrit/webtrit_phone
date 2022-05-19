bcf37bc7242e15bfb98ca2af6c815324b819bd9f
Yurii Cherniavskyi <ych@webtrit.com>
2022-05-19 15:24:27 +0300

    Simplify MainAppBar widget by using inheritance instead of aggregation

--- lib/widgets/main_app_bar.dart
+++ lib/widgets/main_app_bar.dart
@@ -6,55 +6,46 @@ import 'package:go_router/go_router.dart';
 import 'package:webtrit_phone/environment_config.dart';
 import 'package:webtrit_phone/features/features.dart';
 
-class MainAppBar extends StatelessWidget with PreferredSizeWidget {
+class MainAppBar extends AppBar {
   MainAppBar({
     Key? key,
-    this.bottom,
-  })  : preferredSize = Size.fromHeight(kToolbarHeight + (bottom?.preferredSize.height ?? 0.0)),
-        super(key: key);
-
-  final PreferredSizeWidget? bottom;
-
-  @override
-  final Size preferredSize;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text(EnvironmentConfig.APP_NAME),
-      centerTitle: false,
-      actions: [
-        BlocBuilder<CallBloc, CallState>(
-          builder: (context, state) {
-            final themeData = Theme.of(context);
-            return Ink(
-              decoration: ShapeDecoration(
-                shape: CircleBorder(
-                  side: BorderSide(
-                    color: state is ReadyCallStateMixin ? themeData.colorScheme.tertiary : themeData.colorScheme.error,
+    PreferredSizeWidget? bottom,
+  }) : super(
+          key: key,
+          title: const Text(EnvironmentConfig.APP_NAME),
+          centerTitle: false,
+          actions: [
+            BlocBuilder<CallBloc, CallState>(
+              builder: (context, state) {
+                final themeData = Theme.of(context);
+                final colorScheme = themeData.colorScheme;
+                return Ink(
+                  decoration: ShapeDecoration(
+                    shape: CircleBorder(
+                      side: BorderSide(
+                        color: state is ReadyCallStateMixin ? colorScheme.tertiary : colorScheme.error,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              child: IconButton(
-                constraints: const BoxConstraints(
-                  maxWidth: kMinInteractiveDimension,
-                  maxHeight: kMinInteractiveDimension,
-                ),
-                icon: const Icon(
-                  Icons.person,
-                ),
-                onPressed: () {
-                  context.goNamed('settings');
-                },
-              ),
-            );
-          },
-        ),
-        const SizedBox(
-          width: NavigationToolbar.kMiddleSpacing,
-        ),
-      ],
-      bottom: bottom,
-    );
-  }
+                  child: IconButton(
+                    constraints: const BoxConstraints(
+                      maxWidth: kMinInteractiveDimension,
+                      maxHeight: kMinInteractiveDimension,
+                    ),
+                    icon: const Icon(
+                      Icons.person,
+                    ),
+                    onPressed: () {
+                      context.goNamed('settings');
+                    },
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              width: NavigationToolbar.kMiddleSpacing,
+            ),
+          ],
+          bottom: bottom,
+        );
 }
