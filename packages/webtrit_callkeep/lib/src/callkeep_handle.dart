791d79a8b08528776fd5ccc50cc407b36d3e7f23
Yurii Cherniavskyi <ych@webtrit.com>
2022-06-08 11:05:01 +0300

    [callkeep] Add helper constructors and getters

--- packages/webtrit_callkeep/lib/src/callkeep_handle.dart
+++ packages/webtrit_callkeep/lib/src/callkeep_handle.dart
@@ -10,8 +10,20 @@ class CallkeepHandle {
     required this.value,
   });
 
+  const CallkeepHandle.generic(this.value) : type = CallkeepHandleType.generic;
+
+  const CallkeepHandle.number(this.value) : type = CallkeepHandleType.number;
+
+  const CallkeepHandle.email(this.value) : type = CallkeepHandleType.email;
+
   final CallkeepHandleType type;
   final String value;
+
+  bool get isGeneric => type == CallkeepHandleType.generic;
+
+  bool get isNumber => type == CallkeepHandleType.number;
+
+  bool get isEmail => type == CallkeepHandleType.email;
 
   @override
   String toString() {
