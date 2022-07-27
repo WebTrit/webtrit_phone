11e8a215a3b5c22484e47099d9f3d4f1215db97f
Yurii Cherniavskyi <ych@webtrit.com>
2022-07-27 13:44:09 +0300

    Add integration test and flutter driver dependencies with basic test drivers

---
+++ test_driver/integration_extended_screenshot_write_test.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() async {
+  final FlutterDriver driver = await FlutterDriver.connect();
+  await integrationDriver(
+    driver: driver,
+    onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+      final File image = File('$screenshotName.png');
+      image.writeAsBytesSync(screenshotBytes);
+      return true;
+    },
+  );
+}
