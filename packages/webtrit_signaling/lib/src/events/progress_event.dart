3c69ce9ea4bb196a7ac8c595246037a45cf245aa
Yurii Cherniavskyi <ych@webtrit.com>
2022-10-25 16:41:56 +0300

    Make jsep required for progress event

--- packages/webtrit_signaling/lib/src/events/progress_event.dart
+++ packages/webtrit_signaling/lib/src/events/progress_event.dart
@@ -8,12 +8,12 @@ class ProgressEvent extends CallEvent {
     required String callId,
     required this.callee,
     this.isFocus,
-    this.jsep,
+    required this.jsep,
   }) : super(transaction: transaction, line: line, callId: callId);
 
   final String callee;
   final bool? isFocus;
-  final Map<String, dynamic>? jsep;
+  final Map<String, dynamic> jsep;
 
   @override
   List<Object?> get props => [
