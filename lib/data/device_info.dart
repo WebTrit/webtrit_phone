034338c378c06980f9554932d1ff487713edff1b
Yurii Cherniavskyi <ych@webtrit.com>
2022-11-17 22:20:19 +0200

    Update device info dependency to device info plus one
    Also, logging collected device info data.

--- lib/data/device_info.dart
+++ lib/data/device_info.dart
@@ -1,42 +1,193 @@
-import 'dart:io' show Platform;
+import 'dart:io';
 
-import 'package:device_info/device_info.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
-abstract class DeviceInfo {
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:logging/logging.dart';
+
+class DeviceInfo {
   static late DeviceInfo _instance;
 
   static Future<void> init() async {
-    final deviceInfo = DeviceInfoPlugin();
-    if (Platform.isAndroid) {
-      _instance = _AndroidDeviceInfo(await deviceInfo.androidInfo);
-    } else if (Platform.isIOS) {
-      _instance = _IosDeviceInfo(await deviceInfo.iosInfo);
-    } else {
-      throw UnsupportedError('platform not supported');
+    final deviceInfoPlugin = DeviceInfoPlugin();
+
+    late final Map<String, dynamic> deviceData;
+    try {
+      if (kIsWeb) {
+        deviceData = _readWebBrowserInfo(await deviceInfoPlugin.webBrowserInfo);
+      } else {
+        if (Platform.isAndroid) {
+          deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+        } else if (Platform.isIOS) {
+          deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+        } else if (Platform.isLinux) {
+          deviceData = _readLinuxDeviceInfo(await deviceInfoPlugin.linuxInfo);
+        } else if (Platform.isMacOS) {
+          deviceData = _readMacOsDeviceInfo(await deviceInfoPlugin.macOsInfo);
+        } else if (Platform.isWindows) {
+          deviceData = _readWindowsDeviceInfo(await deviceInfoPlugin.windowsInfo);
+        } else {
+          deviceData = <String, dynamic>{
+            'error': 'unknown platform',
+          };
+        }
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'error': 'failed to get platform version',
+      };
     }
+    _instance = DeviceInfo._(deviceData);
   }
 
   factory DeviceInfo() {
     return _instance;
   }
 
-  String get identifierForVendor;
-}
+  DeviceInfo._(this.data) {
+    for (final entry in data.entries) {
+      _logger.finest(() => '${entry.key}: ${entry.value}');
+    }
+  }
 
-class _AndroidDeviceInfo implements DeviceInfo {
-  _AndroidDeviceInfo(this.androidDeviceInfo);
+  final Map<String, dynamic> data;
 
-  final AndroidDeviceInfo androidDeviceInfo;
+  final Logger _logger = Logger('$DeviceInfo');
 
-  @override
-  String get identifierForVendor => androidDeviceInfo.androidId;
-}
+  static Map<String, dynamic> _readWebBrowserInfo(WebBrowserInfo data) {
+    return <String, dynamic>{
+      'browserName': describeEnum(data.browserName),
+      'appCodeName': data.appCodeName,
+      'appName': data.appName,
+      'appVersion': data.appVersion,
+      'deviceMemory': data.deviceMemory,
+      'language': data.language,
+      'languages': data.languages,
+      'platform': data.platform,
+      'product': data.product,
+      'productSub': data.productSub,
+      'userAgent': data.userAgent,
+      'vendor': data.vendor,
+      'vendorSub': data.vendorSub,
+      'hardwareConcurrency': data.hardwareConcurrency,
+      'maxTouchPoints': data.maxTouchPoints,
+    };
+  }
 
-class _IosDeviceInfo implements DeviceInfo {
-  _IosDeviceInfo(this.iodDeviceInfo);
+  static Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+      'displaySizeInches': ((build.displayMetrics.sizeInches * 10).roundToDouble() / 10),
+      'displayWidthPixels': build.displayMetrics.widthPx,
+      'displayWidthInches': build.displayMetrics.widthInches,
+      'displayHeightPixels': build.displayMetrics.heightPx,
+      'displayHeightInches': build.displayMetrics.heightInches,
+      'displayXDpi': build.displayMetrics.xDpi,
+      'displayYDpi': build.displayMetrics.yDpi,
+    };
+  }
 
-  final IosDeviceInfo iodDeviceInfo;
+  static Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
 
-  @override
-  String get identifierForVendor => iodDeviceInfo.identifierForVendor;
+  static Map<String, dynamic> _readLinuxDeviceInfo(LinuxDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'version': data.version,
+      'id': data.id,
+      'idLike': data.idLike,
+      'versionCodename': data.versionCodename,
+      'versionId': data.versionId,
+      'prettyName': data.prettyName,
+      'buildId': data.buildId,
+      'variant': data.variant,
+      'variantId': data.variantId,
+      'machineId': data.machineId,
+    };
+  }
+
+  static Map<String, dynamic> _readMacOsDeviceInfo(MacOsDeviceInfo data) {
+    return <String, dynamic>{
+      'computerName': data.computerName,
+      'hostName': data.hostName,
+      'arch': data.arch,
+      'model': data.model,
+      'kernelVersion': data.kernelVersion,
+      'osRelease': data.osRelease,
+      'activeCPUs': data.activeCPUs,
+      'memorySize': data.memorySize,
+      'cpuFrequency': data.cpuFrequency,
+      'systemGUID': data.systemGUID,
+    };
+  }
+
+  static Map<String, dynamic> _readWindowsDeviceInfo(WindowsDeviceInfo data) {
+    return <String, dynamic>{
+      'numberOfCores': data.numberOfCores,
+      'computerName': data.computerName,
+      'systemMemoryInMegabytes': data.systemMemoryInMegabytes,
+      'userName': data.userName,
+      'majorVersion': data.majorVersion,
+      'minorVersion': data.minorVersion,
+      'buildNumber': data.buildNumber,
+      'platformId': data.platformId,
+      'csdVersion': data.csdVersion,
+      'servicePackMajor': data.servicePackMajor,
+      'servicePackMinor': data.servicePackMinor,
+      'suitMask': data.suitMask,
+      'productType': data.productType,
+      'reserved': data.reserved,
+      'buildLab': data.buildLab,
+      'buildLabEx': data.buildLabEx,
+      'digitalProductId': data.digitalProductId,
+      'displayVersion': data.displayVersion,
+      'editionId': data.editionId,
+      'installDate': data.installDate,
+      'productId': data.productId,
+      'productName': data.productName,
+      'registeredOwner': data.registeredOwner,
+      'releaseId': data.releaseId,
+      'deviceId': data.deviceId,
+    };
+  }
 }
