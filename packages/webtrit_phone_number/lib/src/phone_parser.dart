ba8b8a55cabf1fc20d817b3c41eb909a9e830a43
Yurii Cherniavskyi <ych@webtrit.com>
2022-12-06 14:25:22 +0200

    [phone_number] Fix typo in variable name

--- packages/webtrit_phone_number/lib/src/phone_parser.dart
+++ packages/webtrit_phone_number/lib/src/phone_parser.dart
@@ -1,7 +1,7 @@
 import 'constants.dart';
 
 abstract class PhoneParser {
-  static String normalize(String unformatedPhoneNumber) {
-    return unformatedPhoneNumber.split('').map((char) => Constants.allNormalizationMappings[char] ?? '').join('');
+  static String normalize(String unformattedPhoneNumber) {
+    return unformattedPhoneNumber.split('').map((char) => Constants.allNormalizationMappings[char] ?? '').join('');
   }
 }
