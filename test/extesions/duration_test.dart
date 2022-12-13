13535e3afd8127665138089e09ae5113adae7273
Yurii Cherniavskyi <ych@webtrit.com>
2022-12-14 01:51:33 +0200

    Fix expect function parameters order

--- test/extesions/duration_test.dart
+++ test/extesions/duration_test.dart
@@ -4,12 +4,12 @@ import 'package:webtrit_phone/extensions/extensions.dart';
 
 void main() {
   test('.format() output two segment for Duration up to hour', () {
-    expect('00:01', const Duration(seconds: 1).format());
-    expect('59:59', const Duration(minutes: 59, seconds: 59).format());
+    expect(const Duration(seconds: 1).format(), equals('00:01'));
+    expect(const Duration(minutes: 59, seconds: 59).format(), equals('59:59'));
   });
 
   test('.format() output three segment for Duration over hour', () {
-    expect('01:00:00', const Duration(hours: 1).format());
-    expect('100:00:00', const Duration(hours: 100).format());
+    expect(const Duration(hours: 1).format(), equals('01:00:00'));
+    expect(const Duration(hours: 100).format(), equals('100:00:00'));
   });
 }
