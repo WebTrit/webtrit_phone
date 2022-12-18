fcb68add39997f35fb343c3204e4448848e6379a
Yurii Cherniavskyi <ych@webtrit.com>
2022-12-18 19:33:00 +0200

    Add stub app database internal data package
    To create package next command was executed:
    $ dart create -t package app_database

---
+++ packages/data/app_database/example/app_database_example.dart
@@ -0,0 +1,6 @@
+import 'package:app_database/app_database.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/data/app_database/lib/app_database.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library app_database;
+
+export 'src/app_database_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
