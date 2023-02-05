cdf0f923423b95ab545339384643f991c5546f67
Yurii Cherniavskyi <ych@webtrit.com>
2023-02-05 14:44:34 +0200

    feat(signaling): extend notify event test

--- packages/webtrit_signaling/test/src/events/call/notify_event_test.dart
+++ packages/webtrit_signaling/test/src/events/call/notify_event_test.dart
@@ -8,8 +8,22 @@ import 'package:webtrit_signaling/src/events/call_event.dart';
 import '../event_jsons.dart';
 
 void main() {
-  test('$NotifyEvent fromJson', () {
-    final notifyEventJson = json.decode(r'''
+  void testFromJson(String description, Map<String, dynamic> actual, NotifyEvent expected) {
+    test(description, () {
+      expect(
+        NotifyEvent.fromJson(actual),
+        equals(expected),
+      );
+      expect(
+        CallEvent.fromJson(actual),
+        equals(expected),
+      );
+    });
+  }
+
+  testFromJson(
+    '$NotifyEvent fromJson 1',
+    json.decode(r'''
     {
       "transaction": "transaction 1",
       "line": 0,
@@ -20,9 +34,8 @@ void main() {
       "content_type": "message/sipfrag",
       "content": "SIP/2.0 100 Trying\r\n"
     }
-    ''') as Map<String, dynamic>;
-
-    final notifyEvent = NotifyEvent(
+    ''') as Map<String, dynamic>,
+    NotifyEvent(
       transaction: 'transaction 1',
       line: 0,
       callId: 'qwerty',
@@ -30,17 +43,31 @@ void main() {
       subscriptionState: SubscriptionState.active,
       contentType: 'message/sipfrag',
       content: 'SIP/2.0 100 Trying\r\n',
-    );
+    ),
+  );
 
-    expect(
-      NotifyEvent.fromJson(notifyEventJson),
-      equals(notifyEvent),
-    );
-    expect(
-      CallEvent.fromJson(notifyEventJson),
-      equals(notifyEvent),
-    );
-  });
+  testFromJson(
+    '$NotifyEvent fromJson 2',
+    json.decode(r'''
+    {
+      "line": 0,
+      "call_id": "qwerty",
+      "event": "notify",
+      "notify": "refer",
+      "subscription_state": "active",
+      "content_type": "message/sipfrag",
+      "content": "SIP/2.0 100 Trying\r\n"
+    }
+    ''') as Map<String, dynamic>,
+    NotifyEvent(
+      line: 0,
+      callId: 'qwerty',
+      notify: 'refer',
+      subscriptionState: SubscriptionState.active,
+      contentType: 'message/sipfrag',
+      content: 'SIP/2.0 100 Trying\r\n',
+    ),
+  );
 
   test('$NotifyEvent fromJson error', () {
     expect(
