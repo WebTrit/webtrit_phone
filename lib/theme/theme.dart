96cfa0d7a2ec7990d1c3c3a9859abcae466aa149
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-03 13:52:06 +0300

    refactor: remove deprecate files

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,7 +3,6 @@ export 'elevated_button_styles.dart';
 export 'gen_images.dart';
 export 'gradients.dart';
 export 'input_decorations.dart';
-export 'model/style_model.dart';
 export 'outlined_button_styles.dart';
 export 'portaone.dart';
 export 'text_button_styles.dart';
