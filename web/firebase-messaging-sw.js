0606710958e2720aa7c5c3d6a2b5341f38e79da3
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-08 15:07:28 +0300

    fix: add firebase config for web

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDdWVaDGe0mPRkngXQEUsS1_EQiFyXdoxE',
+  appId: '1:524910534863:web:8ee29ada8594349e4ad7ba',
+  messagingSenderId: '524910534863',
+  projectId: 'webtrit-69559',
+  authDomain: 'webtrit-69559.firebaseapp.com',
+  storageBucket: 'webtrit-69559.appspot.com',
+  measurementId: 'G-2EWYFX6650',
+});
