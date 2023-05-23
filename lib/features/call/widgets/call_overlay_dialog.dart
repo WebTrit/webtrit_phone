5745e08fa42e4c7f67321e70e45bc7c2137e3914
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-23 18:08:36 +0300

    refactor: migrate from single overlay to list of overlay

--- lib/features/call/widgets/call_overlay_dialog.dart
+++ lib/features/call/widgets/call_overlay_dialog.dart
@@ -12,7 +12,6 @@ class CallOverlayDialog extends StatefulWidget {
     required this.createdTime,
     required this.name,
     required this.remoteStream,
-    required this.constraints,
     required this.isVideoCall,
     required this.onEndCall,
   });
@@ -20,7 +19,6 @@ class CallOverlayDialog extends StatefulWidget {
   final DateTime createdTime;
   final MediaStream? remoteStream;
   final String name;
-  final BoxConstraints constraints;
   final bool isVideoCall;
   final Function onEndCall;
 
@@ -64,97 +62,94 @@ class _CallOverlayDialogState extends State<CallOverlayDialog> {
       builder: (context, AsyncSnapshot<List<void>> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           _remoteRenderer.srcObject = widget.remoteStream;
-          return ConstrainedBox(
-            constraints: widget.constraints,
-            child: Wrap(
-              children: [
-                Card(
-                  color: Theme.of(context).colorScheme.primary,
+          return Wrap(
+            children: [
+              Card(
+                color: Theme.of(context).colorScheme.primary,
+                margin: EdgeInsets.zero,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    topRight: Radius.circular(8),
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    widget.isVideoCall
+                        ? Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              color: theme.colorScheme.background,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(90),
+                              ),
+                            ),
+                            width: 64,
+                            height: 64,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(90.0),
+                              child: RTCVideoView(
+                                _remoteRenderer,
+                                objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                              ),
+                            ),
+                          )
+                        : LeadingAvatar(
+                            username: widget.name,
+                          ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    DurationTimer(
+                      createdTime: widget.createdTime,
+                    ),
+                    Text(
+                      widget.name,
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.bodyMedium!.copyWith(
+                        fontFeatures: [
+                          const FontFeature.tabularFigures(),
+                        ],
+                        color: Theme.of(context).colorScheme.background,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Divider(
+                      color: Colors.white54,
+                      height: 0,
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () => widget.onEndCall.call(),
+                child: Card(
                   margin: EdgeInsets.zero,
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      topRight: Radius.circular(8),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
                     ),
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      widget.isVideoCall
-                          ? Container(
-                              padding: const EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                color: theme.colorScheme.background,
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(90),
-                                ),
-                              ),
-                              width: 64,
-                              height: 64,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(90.0),
-                                child: RTCVideoView(
-                                  _remoteRenderer,
-                                  objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                                ),
-                              ),
-                            )
-                          : LeadingAvatar(
-                              username: widget.name,
-                            ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      DurationTimer(
-                        createdTime: widget.createdTime,
-                      ),
-                      Text(
-                        widget.name,
-                        textAlign: TextAlign.center,
-                        style: theme.textTheme.bodyMedium!.copyWith(
-                          fontFeatures: [
-                            const FontFeature.tabularFigures(),
-                          ],
-                          color: Theme.of(context).colorScheme.background,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      const Divider(
-                        color: Colors.white54,
-                        height: 0,
+                  color: Theme.of(context).colorScheme.error,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.call_end_rounded,
+                        color: Colors.white,
                       ),
                     ],
                   ),
                 ),
-                GestureDetector(
-                  onTap: () => widget.onEndCall.call(),
-                  child: Card(
-                    margin: EdgeInsets.zero,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(8),
-                        bottomRight: Radius.circular(8),
-                      ),
-                    ),
-                    color: Theme.of(context).colorScheme.error,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.call_end_rounded,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           );
         } else {
           return const CircularProgressIndicator();
