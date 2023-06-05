a98471ee057272c22c8a6948d564088ad172e28c
Dmitro Serdun <d.serdun@webtrit.com>
2023-06-05 16:25:21 +0300

    refactor(overlay_floating): update collision logic

--- packages/webtrit_overlay_floating/lib/src/overlay_entry_data.dart
+++ packages/webtrit_overlay_floating/lib/src/overlay_entry_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 
+import 'overlay_collision_utils.dart';
 import 'overlay_config.dart';
 import 'overlay_position.dart';
 import 'offset_extension.dart';
@@ -7,21 +8,28 @@ import 'offset_extension.dart';
 class OverlayEntryData {
   OverlayEntryData({
     required Widget child,
-    required this.offset,
-    this.config = const OverlayConfig(),
+    required Offset offset,
+    required OverlayConfig config,
     Widget? minimizedChild,
-  }) {
+  })  : _offset = offset,
+        _config = config {
     entry = _createOverlayEntry(minimizedChild, child);
   }
 
   Widget? widget;
-
-  Offset offset;
   OverlayEntry? entry;
 
-  OverlayPosition overlayPosition = OverlayPosition.center;
+  Offset _offset;
+  OverlayPosition _overlayPosition = OverlayPosition.center;
 
-  OverlayConfig config;
+  final OverlayCollisionUtils _collisionUtils = OverlayCollisionUtils();
+  final OverlayConfig _config;
+
+  get config => _config;
+
+  get offset => _offset;
+
+  get position => _overlayPosition;
 
   void removeOverlayIfExit() {
     entry?.remove();
@@ -32,14 +40,14 @@ class OverlayEntryData {
   OverlayEntry _createOverlayEntry(Widget? minimizedChild, Widget child) {
     return OverlayEntry(
       builder: (context) => Positioned(
-        top: offset.dy,
-        left: offset.dx,
+        top: _offset.dy,
+        left: _offset.dx,
         child: GestureDetector(
           behavior: HitTestBehavior.translucent,
-          onPanUpdate: (details) => _updateOverlayOffset(entry!, details, context),
+          onPanUpdate: (details) => _config.draggability ? _updateOverlayOffset(entry!, details, context) : null,
           child: ConstrainedBox(
             constraints: _prepareConstraints(),
-            child: _prepareOverlayWidget(minimizedChild, child),
+            child: _prepareOverlayWidget(child, minimizedChild),
           ),
         ),
       ),
@@ -47,72 +55,78 @@ class OverlayEntryData {
   }
 
   BoxConstraints _prepareConstraints() {
-    return overlayPosition != OverlayPosition.center
-        ? config.overlayEntityConstraintsMinimized
-        : config.overlayEntityConstraints;
+    return _overlayPosition != OverlayPosition.center
+        ? _config.overlayEntityConstraintsMinimized ?? _config.overlayEntityConstraints
+        : _config.overlayEntityConstraints;
   }
 
-  Widget _prepareOverlayWidget(Widget? minimizedChild, Widget child) {
-    return overlayPosition != OverlayPosition.center
-        ? ClipRRect(
+  Widget _prepareOverlayWidget(
+    Widget child,
+    Widget? minimizedChild,
+  ) {
+    assert(
+        !(minimizedChild != null && _config.overlayEntityConstraintsMinimized == null ||
+            minimizedChild == null && _config.overlayEntityConstraintsMinimized != null),
+        'For a minimized widget, the size must be specified in the config');
+
+    return _overlayPosition == OverlayPosition.center || minimizedChild == null
+        ? child
+        : ClipRRect(
             borderRadius: BorderRadius.horizontal(
-              left: Radius.circular(overlayPosition == OverlayPosition.left ? 0 : config.clipRadius),
-              right: Radius.circular(overlayPosition == OverlayPosition.right ? 0 : config.clipRadius),
+              left: Radius.circular(_overlayPosition == OverlayPosition.left ? 0 : _config.clipRadius),
+              right: Radius.circular(_overlayPosition == OverlayPosition.right ? 0 : _config.clipRadius),
             ),
-            child: minimizedChild ?? child,
-          )
-        : child;
+            child: minimizedChild,
+          );
   }
 
   void _updateOverlayOffset(OverlayEntry overlayEntry, DragUpdateDetails details, BuildContext context) {
-    final overlayWidth = config.overlayEntityConstraints.maxWidth;
-    final overlayHeight = config.overlayEntityConstraints.maxWidth;
-    final overlayWidthMinimized = config.overlayEntityConstraintsMinimized.maxWidth;
+    final overlayWidth = _config.overlayEntityConstraints.maxWidth;
+    final overlayHeight = _config.overlayEntityConstraints.maxHeight;
 
-    final topLeftPosition = offset + details.delta;
+    final topLeftPosition = _offset + details.delta;
     final topRightPosition = topLeftPosition + Offset(overlayWidth, 0);
     final bottomPosition = topLeftPosition + Offset(0, overlayHeight);
 
-    final topRightPositionMinimized = offset + details.delta + Offset(overlayWidthMinimized, 0);
-
     final screenBound = MediaQuery.of(context).size;
 
-    final isMinimized = overlayPosition != OverlayPosition.center;
-    final distanceToRight = screenBound.width - (isMinimized ? topRightPositionMinimized.dx : topRightPosition.dx);
+    if (_config.overlayEntityConstraintsMinimized != null) {
+      final overlayWidthMinimized = _config.overlayEntityConstraintsMinimized?.maxWidth;
+      final topRightPositionMinimized = _offset + details.delta + Offset(overlayWidthMinimized!, 0);
 
-    var outputOffset = offset + details.delta;
+      final collision = _collisionUtils.calculateCollisionForMinimizedState(
+        screenBound: screenBound,
+        offset: _offset,
+        delta: details.delta,
+        bottomPosition: bottomPosition,
+        topRightPosition: topRightPosition,
+        topLeftPosition: topLeftPosition,
+        topRightPositionMinimized: topRightPositionMinimized,
+        overlayWidth: overlayWidth,
+        overlayHeight: overlayHeight,
+        overlayWidthMinimized: overlayWidthMinimized,
+        sticking: _config.sticking,
+        position: _overlayPosition,
+      );
 
-    // Collision check for left side
-    if (topLeftPosition.dx <= config.sticking) {
-      outputOffset = topLeftPosition.copyWith(dx: 0);
-      overlayPosition = OverlayPosition.left;
+      _offset = collision.$1;
+      _overlayPosition = collision.$2;
+    } else {
+      final collision = _collisionUtils.calculateCollision(
+        screenBound: screenBound,
+        offset: _offset,
+        delta: details.delta,
+        bottomPosition: bottomPosition,
+        topRightPosition: topRightPosition,
+        topLeftPosition: topLeftPosition,
+        overlayWidth: overlayWidth,
+        overlayHeight: overlayHeight,
+        sticking: _config.sticking,
+      );
+
+      _offset = collision.$1;
+      _overlayPosition = collision.$2;
     }
-    // Collision check for top side
-    if (topLeftPosition.dy <= 0) {
-      outputOffset = outputOffset.copyWith(dy: 0);
-    }
-
-    // Collision check for the right side
-    if (distanceToRight <= config.sticking) {
-      outputOffset = topRightPosition.copyWith(dx: screenBound.width - overlayWidthMinimized);
-      overlayPosition = OverlayPosition.right;
-    }
-
-    // Collision check for bottom side
-    if (bottomPosition.dy >= screenBound.height) {
-      outputOffset = outputOffset.copyWith(dy: screenBound.height - overlayHeight);
-    }
-
-    // Check whether the overlay has no collision with parties
-    if (topLeftPosition.dx > config.sticking && distanceToRight > config.sticking) {
-      // We check if it was sticking to the right side, if so we compensate the difference for normal logic
-      if (overlayPosition == OverlayPosition.right) {
-        outputOffset = offset + details.delta - Offset(overlayWidth - overlayWidthMinimized, 0);
-      }
-      overlayPosition = OverlayPosition.center;
-    }
-
-    offset = outputOffset;
     overlayEntry.markNeedsBuild();
   }
 }
