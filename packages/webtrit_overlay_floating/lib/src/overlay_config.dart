a38cccc3813f9f967cf2650e6510c0e900fe5cf8
Dmitro Serdun <d.serdun@webtrit.com>
2023-06-05 16:25:21 +0300

    refactor(overlay_floating): update collision logic

--- packages/webtrit_overlay_floating/lib/src/overlay_config.dart
+++ packages/webtrit_overlay_floating/lib/src/overlay_config.dart
@@ -2,15 +2,18 @@ import 'package:flutter/widgets.dart';
 
 class OverlayConfig {
   final BoxConstraints overlayEntityConstraints;
-  final BoxConstraints overlayEntityConstraintsMinimized;
+  final BoxConstraints? overlayEntityConstraintsMinimized;
 
   final double sticking;
   final double clipRadius;
 
+  final bool draggability;
+
   const OverlayConfig({
-    this.overlayEntityConstraints = const BoxConstraints(maxHeight: 148, maxWidth: 148),
-    this.overlayEntityConstraintsMinimized = const BoxConstraints(maxHeight: 148, maxWidth: 40),
+    required this.overlayEntityConstraints,
+    this.overlayEntityConstraintsMinimized,
     this.sticking = 4,
     this.clipRadius = 8,
+    this.draggability = true,
   });
 }
