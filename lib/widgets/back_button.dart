c20722b54e518e2c2c1f464ed962859839825c05
Yurii Cherniavskyi <ych@webtrit.com>
2023-06-08 00:28:24 +0300

    revert: add default colour for ExtBackButton
    The ExtBackButton widget is intended to be used with the AppBar widget,
    and thus should inherit its color from the AppBar.

--- lib/widgets/back_button.dart
+++ lib/widgets/back_button.dart
@@ -4,16 +4,13 @@ class ExtBackButton extends StatelessWidget {
   const ExtBackButton({
     Key? key,
     this.disabled = false,
-    this.color,
   }) : super(key: key);
 
   final bool disabled;
-  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      color: color,
       icon: const Icon(
         Icons.arrow_back,
       ),
