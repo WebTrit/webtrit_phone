0bb6f629ec46320e9cfcfd114894bec86b7f6af2
Yurii Cherniavskyi <ych@webtrit.com>
2023-06-12 01:15:33 +0300

    feat: add logging for received background messages
    Links:
    - https://firebase.google.com/docs/cloud-messaging/flutter/receive

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
+// Copy form `web` static const in `lib/firebase_options.dart`
 firebase.initializeApp({
   apiKey: 'AIzaSyDdWVaDGe0mPRkngXQEUsS1_EQiFyXdoxE',
   appId: '1:524910534863:web:8ee29ada8594349e4ad7ba',
@@ -9,4 +10,12 @@ firebase.initializeApp({
   authDomain: 'webtrit-69559.firebaseapp.com',
   storageBucket: 'webtrit-69559.appspot.com',
   measurementId: 'G-2EWYFX6650',
+});
+
+// Necessary to receive background messages:
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((m) => {
+  console.log("onBackgroundMessage", m);
 });
