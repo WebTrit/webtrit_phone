c3f41cf7ea5290fbff453c3af0000c1c4755c1de
Dmitro Serdun <d.serdun@webtrit.com>
2023-07-27 12:10:20 +0300

    style(style): remove relative imports

--- packages/style/lib/src/dto/dto.dart
+++ packages/style/lib/src/dto/dto.dart
@@ -1,6 +1,6 @@
-export '../dto/application/application_dto.dart';
-export '../dto/color/color_dto.dart';
-export '../dto/image/image_collection_dto.dart';
-export '../dto/image/image_dto.dart';
-export '../dto/texts/texts.dart';
-export '../dto/theme/theme_dto.dart';
+export 'application/application_dto.dart';
+export 'color/color_dto.dart';
+export 'image/image_collection_dto.dart';
+export 'image/image_dto.dart';
+export 'texts/texts.dart';
+export 'theme/theme_dto.dart';
