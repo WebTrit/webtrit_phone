ff432d6e14a4ceda9403c0b478b91877cb7ca5b5
Dmitro Serdun <d.serdun@webtrit.com>
2023-07-31 18:35:02 +0300

    refactor: update data models by new webtrit_api

---
+++ packages/data/app_database/lib/src/migrations/migration_v3.dart
@@ -0,0 +1,13 @@
+import 'package:drift/drift.dart';
+
+import '../app_database.dart';
+import '../migration.dart';
+
+class MigrationV3 extends Migration {
+  const MigrationV3();
+
+  @override
+  Future<void> execute(AppDatabase db, Migrator m) async {
+    await m.renameColumn(db.contactsTable, 'display_name', db.contactsTable.aliasName);
+  }
+}
