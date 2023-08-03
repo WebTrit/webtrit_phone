66903543c632040a2414782dd4ca7d749f80ff11
Dmitro Serdun <d.serdun@webtrit.com>
2023-08-03 15:09:12 +0300

    feat: add exception handler for invalid token exception

--- lib/features/main/view/main_screen.dart
+++ lib/features/main/view/main_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'package:webtrit_api/webtrit_api.dart';
 
 import 'package:webtrit_phone/blocs/blocs.dart';
+import 'package:webtrit_phone/data/data.dart';
 
 import '../main.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({
     Key? key,
     required this.body,
@@ -18,16 +22,45 @@ class MainScreen extends StatelessWidget {
   final ValueChanged<int>? onNavigationBarTap;
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late final _exceptionHandler = Provider.of<ExceptionHandler>(context, listen: false);
+
+  @override
+  void initState() {
+    super.initState();
+    _exceptionHandler.addFilteredListener(_handleInvalidTokenException, filter: (value) => value is RequestFailure);
+  }
+
+  @override
+  void dispose() {
+    _exceptionHandler.removeListener(_handleInvalidTokenException);
+    super.dispose();
+  }
+
+//TODO: Not sure that this logic has a place here
+  void _handleInvalidTokenException() {
+    final appBloc = context.read<AppBloc>();
+    final exception = _exceptionHandler.exceptionModel?.error as RequestFailure?;
+    if (exception?.statusCode == 401) {
+      appBloc.add(const AppLogouted());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
+
     final scaffold = Scaffold(
-      body: body,
+      body: widget.body,
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         selectedLabelStyle: themeData.textTheme.bodySmall,
         unselectedLabelStyle: themeData.textTheme.bodySmall,
-        currentIndex: navigationBarFlavor.index,
-        onTap: onNavigationBarTap,
+        currentIndex: widget.navigationBarFlavor.index,
+        onTap: widget.onNavigationBarTap,
         items: MainFlavor.values.map((flavor) {
           return BottomNavigationBarItem(
             icon: Icon(flavor.icon),
