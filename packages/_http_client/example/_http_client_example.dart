46aae2e6a126438099515af3a8dbf1b9685c4331
Yurii Cherniavskyi <ych@webtrit.com>
2023-08-07 19:55:08 +0300

    feat: add internal package to create platform-appropriate HTTP clients
    $ dart create --template package _http_client

---
+++ packages/_http_client/example/_http_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:_http_client/_http_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/_http_client/test/_http_client_test.dart
@@ -0,0 +1,16 @@
+import 'package:_http_client/_http_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
