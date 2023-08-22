c468871f5b6c7db9bc0f92e0db3322270e389794
Dmitro Serdun <d.serdun@webtrit.com>
2023-08-22 19:27:56 +0300

    feat: update to new app IDs and refine native project structures with Dart define handling (#36)
    Links:
    https://itnext.io/secure-your-flutter-project-the-right-way-to-set-environment-variables-with-compile-time-variables-67c3163ff9f4
    
    Co-authored-by: Yurii Cherniavskyi <ych@webtrit.com>

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -4,57 +4,56 @@ class EnvironmentConfig {
   EnvironmentConfig._();
 
   static const DEBUG_LEVEL = String.fromEnvironment(
-    'WEBTRIT_PHONE_DEBUG_LEVEL',
+    'WEBTRIT_APP_DEBUG_LEVEL',
     defaultValue: 'INFO',
   );
 
   static const DATABASE_LOG_STATEMENTS = bool.fromEnvironment(
-    'WEBTRIT_PHONE_DATABASE_LOG_STATEMENTS',
+    'WEBTRIT_APP_DATABASE_LOG_STATEMENTS',
     defaultValue: false,
   );
 
   static const PERIODIC_POLLING = bool.fromEnvironment(
-    'WEBTRIT_PHONE_PERIODIC_POLLING',
+    'WEBTRIT_APP_PERIODIC_POLLING',
     defaultValue: true,
   );
 
   static const CORE_URL = String.fromEnvironment(
-    'WEBTRIT_PHONE_WEBTRIT_CORE_URL',
+    'WEBTRIT_APP_CORE_URL',
   );
 
   static const DEMO_CORE_URL = String.fromEnvironment(
-    'WEBTRIT_PHONE_DEMO_WEBTRIT_CORE_URL',
+    'WEBTRIT_APP_DEMO_CORE_URL',
     defaultValue: 'http://localhost:4000',
   );
 
   static const APP_NAME = String.fromEnvironment(
-    'WEBTRIT_PHONE_APP_NAME',
+    'WEBTRIT_APP_NAME',
     defaultValue: 'WebTrit',
   );
 
-  static const SALES_EMAIL = String.fromEnvironment(
-    'WEBTRIT_PHONE_SALES_EMAIL',
-    defaultValue: 'sales@webtrit.com',
-  );
-
   static const APP_DESCRIPTION = String.fromEnvironment(
-    'WEBTRIT_PHONE_APP_DESCRIPTION',
-    defaultValue: '$APP_NAME application',
+    'WEBTRIT_APP_DESCRIPTION',
   );
 
   static const APP_HELP_URL = String.fromEnvironment(
-    'WEBTRIT_PHONE_APP_HELP_URL',
+    'WEBTRIT_APP_HELP_URL',
   );
 
   static const APP_ABOUT_URL = String.fromEnvironment(
-    'WEBTRIT_PHONE_APP_ABOUT_URL',
+    'WEBTRIT_APP_ABOUT_URL',
   );
 
   static const APP_TERMS_AND_CONDITIONS_URL = String.fromEnvironment(
-    'WEBTRIT_PHONE_APP_TERMS_AND_CONDITIONS_URL',
+    'WEBTRIT_APP_TERMS_AND_CONDITIONS_URL',
+  );
+
+  static const SALES_EMAIL = String.fromEnvironment(
+    'WEBTRIT_APP_SALES_EMAIL',
+    defaultValue: 'sales@webtrit.com',
   );
 
   static const FCM_VAPID_KEY = String.fromEnvironment(
-    'WEBTRIT_PHONE_FCM_VAPID_KEY',
+    'WEBTRIT_APP_FCM_VAPID_KEY',
   );
 }
