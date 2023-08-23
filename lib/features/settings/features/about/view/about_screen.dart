8611111000aafa8f070387c50755a370fbd5e5b9
Yurii Cherniavskyi <ych@webtrit.com>
2023-08-23 14:39:23 +0300

    refactor: remove unused import

--- lib/features/settings/features/about/view/about_screen.dart
+++ lib/features/settings/features/about/view/about_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 
 import 'package:webtrit_phone/l10n/l10n.dart';
 import 'package:webtrit_phone/theme/theme.dart';
