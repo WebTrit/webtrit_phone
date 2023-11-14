9cc1005ebcffd5e221266f4710066910a66a1391
Dmitro Serdun <dmitriiserdun@gmail.com>
2023-11-14 16:33:55 +0200

    feat: add deferred deep links for android

--- lib/data/app_deep_links.dart
+++ lib/data/app_deep_links.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:android_play_install_referrer/android_play_install_referrer.dart';
 import 'package:app_links/app_links.dart';
 
 class AppDeepLinks with ChangeNotifier {
@@ -7,15 +8,21 @@ class AppDeepLinks with ChangeNotifier {
 
   static Future<void> init() async {
     final appLinks = AppLinks();
+    final referrerDetails = await AndroidPlayInstallReferrer.installReferrer;
     final initialLink = await appLinks.getLatestAppLink();
-    _instance = AppDeepLinks._(initialLink, appLinks);
+    _instance = AppDeepLinks._(initialLink, appLinks, referrerDetails);
   }
 
   factory AppDeepLinks() {
     return _instance;
   }
 
-  AppDeepLinks._(this._initialUrl, AppLinks appLinks) {
+  AppDeepLinks._(Uri? initialUrl, AppLinks appLinks, ReferrerDetails referrerDetails) {
+    final referrerQuery = _parseQueryString(referrerDetails.installReferrer ?? '');
+    final referrerPath = Uri.parse(referrerQuery.remove('path') ?? '').replace(queryParameters: referrerQuery);
+
+    _initialUrl = initialUrl ?? referrerPath;
+
     appLinks.uriLinkStream.listen((uri) {
       _initialUrl = uri;
       notifyListeners();
@@ -25,4 +32,18 @@ class AppDeepLinks with ChangeNotifier {
   Uri? _initialUrl;
 
   Uri? get initialUrl => _initialUrl;
+
+  Map<String, String> _parseQueryString(String queryString) {
+    List<String> pairs = queryString.split('&');
+    Map<String, String> resultMap = {};
+    for (String pair in pairs) {
+      List<String> keyValue = pair.split('=');
+      if (keyValue.length == 2) {
+        String key = Uri.decodeComponent(keyValue[0]);
+        String value = Uri.decodeComponent(keyValue[1]);
+        resultMap[key] = value;
+      }
+    }
+    return resultMap;
+  }
 }
