f9a9b8b13100849f6c7b8aed069cb82ad8540e4d
Dmitro <dmitro.serdun@gmail.com>
2024-02-06 16:48:11 +0200

    feat: move common login widgets to auth package

--- lib/features/login/view/login_core_url_assign_tab.dart
+++ lib/features/login/view/login_core_url_assign_tab.dart
@@ -9,6 +9,7 @@ import 'package:webtrit_phone/extensions/extensions.dart';
 import 'package:webtrit_phone/l10n/l10n.dart';
 import 'package:webtrit_phone/theme/theme.dart';
 import 'package:webtrit_phone/widgets/widgets.dart';
+import 'package:webtrit_phone/features/auth/auth.dart';
 
 import '../login.dart';
 
