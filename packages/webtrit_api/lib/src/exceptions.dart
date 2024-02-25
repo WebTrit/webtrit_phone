8e76ec3a532400356a31357480a7839127592ce8
Dmitro <d.serdun@webtrit.com>
2024-02-25 12:20:03 +0200

    feat: update error model according to the server

--- packages/webtrit_api/lib/src/exceptions.dart
+++ packages/webtrit_api/lib/src/exceptions.dart
@@ -15,8 +15,7 @@ class RequestFailure implements Exception {
     if (error != null) {
       final errorDetails = error.details;
       if (errorDetails != null) {
-        final s = errorDetails.map((d) => '${d.path}: ${d.reason}').join(', ');
-        return '$RequestFailure($statusCode, ${error.code}, [$s])';
+        return '$RequestFailure($statusCode, ${error.code},${error.details?.path}: ${error.details?.reason}';
       } else {
         return '$RequestFailure($statusCode, ${error.code})';
       }
