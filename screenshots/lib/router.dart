50e4f22c98788097e8710940113cd4b6c77139d1
Dmitro Serdun <d.serdun@webtrit.com>
2024-04-05 14:01:11 +0300

    Fixes/screenshots beta (#89)
    * feat (callkeep): migration to new callkeep
    
    * chore (callkeep): restore original path
    
    * fix: clean keypad on call start or transfer CU-86942ky1q
    
    * refactor: extract get and clean number to function _popNumber
    
    * style: revert bloated event creation format
    
    * refactor: rename local variable to shorter form
    
    * fix(screenshots): android minsdk
    
    * fix(screenshots): zone scope requires to run ensureInitialized inside
    
    * fix(screenshots): async test function can't run properly by test_driver
    
    * fix(screenshots): integration test auto_route context fix
    
    * fix(screenshots): go_router remove
    
    * chore(screenshots): suppress warning importing a public library that exports this library
    
    * style(screenshots): grope imports by scope
    
    ---------
    
    Co-authored-by: Vladislav Komelkov <v.komelkov@webtrit.com>
    Co-authored-by: digiborimusik <digiborimusik@gmail.com>
    Co-authored-by: Yurii Cherniavskyi <ych@webtrit.com>

---
+++ screenshots/lib/router.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:webtrit_phone/blocs/app/app_bloc.dart';
+import 'package:webtrit_phone/environment_config.dart';
+import 'package:webtrit_phone/features/main/models/models.dart';
+
+import 'package:screenshots/screenshots/screenshots.dart';
+import 'package:screenshots/widgets/widgets.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Screen,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        RedirectRoute(path: '/', redirectTo: '/0'),
+        AutoRoute(path: '/:$_kInitialIndexParameterName', page: IndexInputRoute.page),
+      ];
+}
+
+@RoutePage()
+class IndexInputScreen extends StatefulWidget {
+  final int index;
+
+  const IndexInputScreen({super.key, @PathParam(_kInitialIndexParameterName) required this.index});
+
+  @override
+  State<IndexInputScreen> createState() => _IndexInputScreenState();
+}
+
+class _IndexInputScreenState extends State<IndexInputScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final appBloc = context.read<AppBloc>();
+
+    late final screenshots = [
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: LoginModeSelectScreenScreenshot(
+          appGreeting: EnvironmentConfig.APP_GREETING.isEmpty ? null : EnvironmentConfig.APP_GREETING,
+        ),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const MainScreenScreenshot(
+          MainFlavor.favorites,
+          Text(EnvironmentConfig.APP_NAME),
+        ),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const MainScreenScreenshot(
+          MainFlavor.recents,
+          Text(EnvironmentConfig.APP_NAME),
+        ),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const MainScreenScreenshot(
+          MainFlavor.keypad,
+          Text(EnvironmentConfig.APP_NAME),
+        ),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const SettingScreenScreenshot(),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const CallScreenScreenshot(false),
+      ),
+      ScreenshotApp(
+        appBloc: appBloc,
+        child: const CallScreenScreenshot(true),
+      ),
+    ];
+
+    return DefaultTabController(
+      key: ValueKey(widget.index),
+      length: screenshots.length,
+      initialIndex: widget.index,
+      child: TabBarView(
+        children: screenshots,
+      ),
+    );
+  }
+}
+
+const _kInitialIndexParameterName = 'initialIndex';
