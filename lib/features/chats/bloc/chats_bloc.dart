e402d3edf758a5eb34f5414a6d688b7bae49364a
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-04-11 21:46:42 +0300

    getstream connection

--- lib/features/chats/bloc/chats_bloc.dart
+++ lib/features/chats/bloc/chats_bloc.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+// ignore: depend_on_referenced_packages
+import 'package:http/http.dart' as http; // TODO: remove
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 part 'chats_event.dart';
@@ -17,7 +20,30 @@ class ChatsBloc extends Bloc<ChatsEvent, ChatsState> {
   void _connect(Connect event, Emitter<ChatsState> emit) async {
     debugPrint(_token);
     debugPrint(_tenantId);
-    await Future.delayed(const Duration(seconds: 2));
-    emit(state.copyWith(status: ChatsStatus.connected));
+
+    try {
+      final chatClientData = await _chatClientData;
+      await state.client.connectUser(User(id: chatClientData.userId), chatClientData.token);
+      emit(state.copyWith(status: ChatsStatus.connected));
+    } on Exception catch (e) {
+      emit(state.copyWith(status: ChatsStatus.error, error: e));
+    }
+  }
+
+  /// Fetches the chat token and user id from the chat service
+  /// TODO: move somewhere
+  Future<ChatClientData> get _chatClientData async {
+    final response = await http.get(
+      Uri.parse('http://192.168.1.85:3000/auth/chat-token'),
+      headers: {'Authorization': 'Bearer $_token', 'x-tenant-id': _tenantId},
+    );
+
+    final responseJson = jsonDecode(response.body);
+    final chatToken = responseJson['token'] as String;
+    final userId = responseJson['userId'] as String;
+
+    return (userId: userId, token: chatToken);
   }
 }
+
+typedef ChatClientData = ({String userId, String token});
--- lib/features/chats/bloc/chats_state.dart
+++ lib/features/chats/bloc/chats_state.dart
@@ -3,15 +3,16 @@ part of 'chats_bloc.dart';
 enum ChatsStatus { initial, error, connecting, connected }
 
 class ChatsState with EquatableMixin {
-  const ChatsState._(this.client, this.status);
+  const ChatsState._(this.client, this.status, this.error);
 
   final StreamChatClient client;
   final ChatsStatus status;
+  final Exception? error;
 
-  factory ChatsState.initial(String apiKey) => ChatsState._(StreamChatClient(apiKey), ChatsStatus.initial);
+  factory ChatsState.initial(String apiKey) => ChatsState._(StreamChatClient(apiKey), ChatsStatus.initial, null);
 
-  ChatsState copyWith({StreamChatClient? client, ChatsStatus? status}) {
-    return ChatsState._(client ?? this.client, status ?? this.status);
+  ChatsState copyWith({ChatsStatus? status, Exception? error}) {
+    return ChatsState._(client, status ?? this.status, error);
   }
 
   @override
