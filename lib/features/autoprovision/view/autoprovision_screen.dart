20a9c61cd2b89d8be8804921210eb7de0653d11f
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-04-17 10:35:11 +0300

    fix: improved autoprovision routing for edge cases

--- lib/features/autoprovision/view/autoprovision_screen.dart
+++ lib/features/autoprovision/view/autoprovision_screen.dart
@@ -9,34 +9,56 @@ import 'package:webtrit_phone/features/autoprovision/cubit/autoprovision_cubit.d
 import 'package:webtrit_phone/features/autoprovision/widgets/relogin_dialog.dart';
 import 'package:webtrit_phone/features/notifications/notifications.dart';
 
-class AutoprovisionScreen extends StatelessWidget {
+class AutoprovisionScreen extends StatefulWidget {
   const AutoprovisionScreen({super.key});
 
   @override
+  State<AutoprovisionScreen> createState() => _AutoprovisionScreenState();
+}
+
+class _AutoprovisionScreenState extends State<AutoprovisionScreen> {
+  late final appBloc = context.read<AppBloc>();
+  late final nfnBloc = context.read<NotificationsBloc>();
+  late final apnCubit = context.read<AutoprovisionCubit>();
+  late final router = context.router;
+
+  navigateBack() async {
+    if (router.canPop(ignorePagelessRoutes: true)) {
+      await router.pop();
+    } else {
+      // For the case when the app is launched with the autoprovision screen as initial route.
+      await router.replaceAll([const AppShellRoute()]);
+    }
+  }
+
+  onError(BuildContext context, Error state) async {
+    await navigateBack();
+    nfnBloc.add(NotificationsMessaged(DefaultErrorNotification(state.error)));
+  }
+
+  onConfirmationNeeded(BuildContext context) async {
+    final result = await showDialog(
+      context: context,
+      builder: (context) => const ReloginDialog(),
+      useRootNavigator: false,
+    );
+
+    if (!mounted) return;
+
+    if (result == true) {
+      apnCubit.confirmReplaceSession();
+    } else {
+      navigateBack();
+    }
+  }
+
+  onSessionCreated(BuildContext context, SessionCreated state) async {
+    await navigateBack();
+    appBloc.add(AppLogined(coreUrl: state.coreUrl, token: state.token, tenantId: state.tenantId ?? ''));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final appBloc = context.read<AppBloc>();
-    final nfnBloc = context.read<NotificationsBloc>();
-    final apnCubit = context.read<AutoprovisionCubit>();
-    final router = context.router;
-
-    onError(BuildContext context, Error state) {
-      router.navigate(const AppShellRoute());
-      nfnBloc.add(NotificationsMessaged(DefaultErrorNotification(state.error)));
-    }
-
-    onConfirmationNeeded(BuildContext context) async {
-      final result = await showDialog(context: context, builder: (context) => const ReloginDialog());
-      if (result == true) {
-        apnCubit.confirmReplaceSession();
-      } else {
-        router.navigate(const AppShellRoute());
-      }
-    }
-
-    onSessionCreated(BuildContext context, SessionCreated state) {
-      appBloc.add(AppLogined(coreUrl: state.coreUrl, token: state.token, tenantId: state.tenantId ?? ''));
-    }
-
     return Scaffold(
       body: BlocConsumer<AutoprovisionCubit, AutoprovisionState>(
         listener: (context, state) {
