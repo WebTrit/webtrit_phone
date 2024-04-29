7d1344582237afdf45d248bd12b78a304db1d3ab
Dmitro Serdun <d.serdun@webtrit.com>
2024-04-29 15:34:08 +0300

    feat: add extensions to get manufacturer tips

---
+++ lib/features/permissions/extensions/manufacturer_tips.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:webtrit_phone/l10n/l10n.dart';
+
+import '../cubit/permissions_cubit.dart';
+
+extension ManufacturerTips on Manufacturer {
+  List<String> tips(BuildContext context) {
+    switch (this) {
+      case Manufacturer.xiaomi:
+        return [
+          context.l10n.permission_manufacturer_Text_xiaomi_tip1,
+          context.l10n.permission_manufacturer_Text_xiaomi_tip2,
+        ];
+    }
+  }
+}
