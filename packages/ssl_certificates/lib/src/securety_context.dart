81c8633d4f69c539c4928affe1251afb7837f201
Dmitro Serdun <d.serdun@webtrit.com>
2024-05-01 11:01:49 +0300

    feat(ssl_certificates): add a module for initializing secure context

---
+++ packages/ssl_certificates/lib/src/securety_context.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'trusted_certificates.dart';
+
+SecurityContext? initializeSecurityContext(TrustedCertificates trustedCertificates) {
+  if (!trustedCertificates.hasAvailableCertificates) return null;
+
+  final securityContext = SecurityContext(withTrustedRoots: true);
+
+  for (final cert in trustedCertificates.certificates) {
+    securityContext.setTrustedCertificatesBytes(
+      cert.bytes,
+      password: cert.password,
+    );
+  }
+
+  return securityContext;
+}
