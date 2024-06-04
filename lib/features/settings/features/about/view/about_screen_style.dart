6f2568c567199ee29b0bae0f50aaef180e205e8e
Vladislav Komelkov <digiborideveloper@gmail.com>
2024-06-04 13:06:42 +0300

    chore: fix linter warnings (#159)

--- lib/features/settings/features/about/view/about_screen_style.dart
+++ lib/features/settings/features/about/view/about_screen_style.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
 import 'package:webtrit_phone/theme/theme.dart';
 
--- lib/theme/extension/logo_assets.dart
+++ lib/theme/extension/logo_assets.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 import '../models/theme_asset.dart';
 
