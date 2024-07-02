cd9dbfad30c410c946e0418f9b545cf6cbb7a8e9
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-07-02 14:05:13 +0300

    fix: wrong ids for removed contacts while sync

--- lib/blocs/local_contacts_sync/local_contacts_sync_bloc.dart
+++ lib/blocs/local_contacts_sync/local_contacts_sync_bloc.dart
@@ -88,7 +88,9 @@ class LocalContactsSyncBloc extends Bloc<LocalContactsSyncEvent, LocalContactsSy
       await appDatabase.transaction(() async {
         final localContacts = event.contacts;
 
-        final syncedLocalContactsIds = await appDatabase.contactsDao.getIdsBySourceType(ContactSourceTypeEnum.local);
+        final contactDatas = await appDatabase.contactsDao.getAllContacts(ContactSourceTypeEnum.local);
+
+        final syncedLocalContactsIds = contactDatas.map((contactData) => contactData.sourceId).toSet();
 
         final updatedLocalContactsIds = localContacts.map((localContact) => localContact.id).toSet();
 
