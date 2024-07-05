6e12bdd9895438ef5309ccb4a22a60db713646a0
Dmitro Serdun <d.serdun@webtrit.com>
2024-07-05 14:18:01 +0300

    feat: add the ability to read the app version from pubspec.yaml by adding it to assets

--- lib/data/app_info.dart
+++ lib/data/app_info.dart
@@ -1,20 +1,39 @@
-import 'package:firebase_app_installations/firebase_app_installations.dart';
+import 'package:flutter/services.dart' as services;
 
-import 'package:webtrit_phone/app/app_version.gen.dart';
+import 'package:firebase_app_installations/firebase_app_installations.dart';
+import 'package:logging/logging.dart';
+
+import 'package:webtrit_phone/app/assets.gen.dart';
+
+final Logger _logger = Logger('AppInfo');
 
 class AppInfo {
   static late AppInfo _instance;
 
   static Future<void> init() async {
     final id = await FirebaseInstallations.instance.getId();
-    _instance = AppInfo._(id);
+
+    String? appVersion = await _getAppVersion();
+
+    _instance = AppInfo._(id, appVersion);
+  }
+
+  static Future<String?> _getAppVersion() async {
+    try {
+      final pubspecString = await services.rootBundle.loadString(Assets.pubspec);
+      final regExp = RegExp(r'app_version:\s*(\d+\.\d+\.\d+(\+\d+)?)');
+      return regExp.firstMatch(pubspecString)?.group(1);
+    } catch (e) {
+      _logger.warning(e);
+      return null;
+    }
   }
 
   factory AppInfo() {
     return _instance;
   }
 
-  AppInfo._(this._identifier) {
+  AppInfo._(this._identifier, this._appVersion) {
     FirebaseInstallations.instance.onIdChange.listen((String id) {
       _identifier = id;
     });
@@ -22,7 +41,9 @@ class AppInfo {
 
   String _identifier;
 
+  String? _appVersion;
+
   String get identifier => _identifier;
 
-  String get version => AppVersion.version;
+  String get version => _appVersion ?? '';
 }
