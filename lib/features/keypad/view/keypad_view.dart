89c3068fac23893fdd59012d74ef55ef3e10c204
Dmitro Serdun <d.serdun@webtrit.com>
2024-07-25 12:20:10 +0300

    feat: add video button to action pad widget

--- lib/features/keypad/view/keypad_view.dart
+++ lib/features/keypad/view/keypad_view.dart
@@ -75,10 +75,9 @@ class KeypadViewState extends State<KeypadView> {
                       previous.isBlingTransferInitiated != current.isBlingTransferInitiated,
                   builder: (context, callState) {
                     return Actionpad(
-                      video: state.video,
                       transfer: callState.isBlingTransferInitiated,
-                      onCallPressed: value.text.isEmpty ? null : () => _onCallPressed(state.video),
-                      onCallLongPress: _onCallLongPress,
+                      onAudioCallPressed: value.text.isEmpty ? null : () => _onCallPressed(false),
+                      onVideoCallPressed: value.text.isEmpty ? null : () => _onCallPressed(true),
                       onTransferPressed: _onTransferPressed,
                       onBackspacePressed: value.text.isEmpty ? null : _onBackspacePressed,
                       onBackspaceLongPress: value.text.isEmpty ? null : _onBackspaceLongPress,
