4364180eeceb1a2ed113fbcf3478bbbf208cee5a
Dmitro Serdun <d.serdun@webtrit.com>
2024-08-08 12:59:01 +0300

    feat: add the option to enable or disable attended transfer in the environment param (#209)
    * feat: add an environment parameter to enable or disable attended transfer
    
    * feat: add call action configuration for managing availability components
    
    * refactor: add condition to check for null attendant callback
    
    * feat: provide active call configuration for view
    
    * refactor: change environment name for enabling attended transfer

--- lib/features/call/models/models.dart
+++ lib/features/call/models/models.dart
@@ -1,5 +1,6 @@
 export 'package:flutter_webrtc/flutter_webrtc.dart' show RTCVideoRenderer;
 
+export 'call_active_config.dart';
 export 'call_display.dart';
 export 'call_status.dart';
 export 'jsep_value.dart';
