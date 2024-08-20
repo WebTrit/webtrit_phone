d8909f24e0a30324d95d1f9db0fe8912ba4f6a0a
Dmitro Serdun <d.serdun@webtrit.com>
2024-08-20 13:12:13 +0300

    refactor(webtrit_signaling): remove the `description` field from the `SignalingResponseCode` enum and add a reference comment

--- packages/webtrit_signaling/lib/src/signaling_response_code.dart
+++ packages/webtrit_signaling/lib/src/signaling_response_code.dart
@@ -9,44 +9,43 @@ enum SignalingResponseCodeType {
   token,
 }
 
+// The meaning of each code can be found at:
+// https://github.com/WebTrit/webtrit_docs/blob/b2148c4011d98f2887a3994b8f63c351632ad9dd/signaling/responses/error_codes.md
 enum SignalingResponseCode {
-  unauthorizedRequest(
-      SignalingResponseCodeType.unauthorized, 403, 'Unauthorized request (wrong or missing secret/token)'),
-  unauthorizedAccess(
-      SignalingResponseCodeType.unauthorized, 405, 'Unauthorized access to plugin (token is not allowed to)'),
-  unknownError(SignalingResponseCodeType.unknown, 490, 'Unknown error'),
-  transportSpecificError(SignalingResponseCodeType.transport, 450, 'Transport specific error'),
-  missingRequest(SignalingResponseCodeType.request, 452, 'Missing request'),
-  unknownRequest(SignalingResponseCodeType.request, 453, 'Unknown request'),
-  invalidJson(SignalingResponseCodeType.request, 454, 'Invalid JSON'),
-  invalidJsonObject(SignalingResponseCodeType.request, 455, 'Invalid JSON Object'),
-  missingMandatoryElement(SignalingResponseCodeType.request, 456, 'Missing mandatory element'),
-  invalidPath(SignalingResponseCodeType.request, 457, 'Invalid path for this request'),
-  sessionNotFound(SignalingResponseCodeType.session, 458, 'Session not found'),
-  handleNotFound(SignalingResponseCodeType.session, 459, 'Handle not found'),
-  pluginNotFound(SignalingResponseCodeType.plugin, 460, 'Plugin not found'),
-  errorAttachingPlugin(SignalingResponseCodeType.plugin, 461, 'Error attaching plugin'),
-  errorSendingMessage(SignalingResponseCodeType.plugin, 462, 'Error sending message to plugin'),
-  errorDetachingPlugin(SignalingResponseCodeType.plugin, 463, 'Error detaching from plugin'),
-  unsupportedJsepType(SignalingResponseCodeType.plugin, 464, 'Unsupported JSEP type'),
-  invalidSdp(SignalingResponseCodeType.plugin, 465, 'Invalid SDP'),
-  invalidStream(SignalingResponseCodeType.plugin, 466, 'Invalid stream'),
-  invalidElementType(SignalingResponseCodeType.plugin, 467, 'Invalid element type'),
-  sessionIdInUse(SignalingResponseCodeType.session, 468, 'Session ID already in use'),
-  unexpectedAnswer(SignalingResponseCodeType.plugin, 469, 'Unexpected ANSWER (no OFFER)'),
-  tokenNotFound(SignalingResponseCodeType.token, 470, 'Token not found'),
-  wrongWebrtcState(SignalingResponseCodeType.webrtc, 471, 'Wrong WebRTC state'),
-  notAcceptingNewSessions(SignalingResponseCodeType.session, 472, 'Currently not accepting new sessions');
+  unauthorizedRequest(SignalingResponseCodeType.unauthorized, 403),
+  unauthorizedAccess(SignalingResponseCodeType.unauthorized, 405),
+  unknownError(SignalingResponseCodeType.unknown, 490),
+  transportSpecificError(SignalingResponseCodeType.transport, 450),
+  missingRequest(SignalingResponseCodeType.request, 452),
+  unknownRequest(SignalingResponseCodeType.request, 453),
+  invalidJson(SignalingResponseCodeType.request, 454),
+  invalidJsonObject(SignalingResponseCodeType.request, 455),
+  missingMandatoryElement(SignalingResponseCodeType.request, 456),
+  invalidPath(SignalingResponseCodeType.request, 457),
+  sessionNotFound(SignalingResponseCodeType.session, 458),
+  handleNotFound(SignalingResponseCodeType.session, 459),
+  pluginNotFound(SignalingResponseCodeType.plugin, 460),
+  errorAttachingPlugin(SignalingResponseCodeType.plugin, 461),
+  errorSendingMessage(SignalingResponseCodeType.plugin, 462),
+  errorDetachingPlugin(SignalingResponseCodeType.plugin, 463),
+  unsupportedJsepType(SignalingResponseCodeType.plugin, 464),
+  invalidSdp(SignalingResponseCodeType.plugin, 465),
+  invalidStream(SignalingResponseCodeType.plugin, 466),
+  invalidElementType(SignalingResponseCodeType.plugin, 467),
+  sessionIdInUse(SignalingResponseCodeType.session, 468),
+  unexpectedAnswer(SignalingResponseCodeType.plugin, 469),
+  tokenNotFound(SignalingResponseCodeType.token, 470),
+  wrongWebrtcState(SignalingResponseCodeType.webrtc, 471),
+  notAcceptingNewSessions(SignalingResponseCodeType.session, 472);
 
-  const SignalingResponseCode(this.type, this.code, this.description);
+  const SignalingResponseCode(this.type, this.code);
 
   final SignalingResponseCodeType type;
   final int code;
-  final String description;
 
   @override
   String toString() {
-    return 'SignalingResponseCode(type: $type, code: $code, description: $description)';
+    return 'SignalingResponseCode(type: $type, code: $code)';
   }
 }
 
