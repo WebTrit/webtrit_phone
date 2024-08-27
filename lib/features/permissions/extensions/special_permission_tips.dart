ee317ac7664b58f14679dfc85fb6081e47618457
Dmitro Serdun <d.serdun@webtrit.com>
2024-08-27 18:59:54 +0300

    feat: implement handling for special permissions

---
+++ lib/features/permissions/extensions/special_permission_tips.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:webtrit_phone/data/data.dart';
+
+import 'package:webtrit_phone/l10n/l10n.dart';
+
+extension SpecialPermissionTips on CallkeepSpecialPermissions {
+  List<String> tips(BuildContext context) {
+    switch (this) {
+      case CallkeepSpecialPermissions.fullScreenIntent:
+        return [
+          context.l10n.permission_manageFullScreenNotificationInstructions_step1,
+          context.l10n.permission_manageFullScreenNotificationInstructions_step2,
+          context.l10n.permission_manageFullScreenNotificationInstructions_step3,
+          context.l10n.permission_manageFullScreenNotificationInstructions_step4,
+          context.l10n.permission_manageFullScreenNotificationInstructions_step5,
+        ];
+    }
+  }
+}
