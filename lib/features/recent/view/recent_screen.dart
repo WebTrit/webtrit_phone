5027afe52f29067065b6a8e73b867e01f9a354aa
Dmitro Serdun <d.serdun@webtrit.com>
2024-08-29 10:55:29 +0300

    feat: add actions for audio calls, video calls to recent calls details screen

--- lib/features/recent/view/recent_screen.dart
+++ lib/features/recent/view/recent_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:webtrit_phone/app/constants.dart';
 import 'package:webtrit_phone/extensions/extensions.dart';
 import 'package:webtrit_phone/features/features.dart';
 import 'package:webtrit_phone/l10n/l10n.dart';
+import 'package:webtrit_phone/models/models.dart';
 import 'package:webtrit_phone/widgets/widgets.dart';
-
-import '../recent.dart';
 
 class RecentScreen extends StatelessWidget {
   const RecentScreen({
@@ -29,6 +29,10 @@ class RecentScreen extends StatelessWidget {
             );
           } else {
             final themeData = Theme.of(context);
+            final buttonStyle = OutlinedButton.styleFrom(
+              side: BorderSide(color: themeData.colorScheme.surfaceTint),
+              elevation: 0,
+            );
             return ListView(
               children: [
                 Padding(
@@ -42,6 +46,29 @@ class RecentScreen extends StatelessWidget {
                   recent.name,
                   style: themeData.textTheme.headlineMedium,
                   textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      style: buttonStyle,
+                      child: const Icon(Icons.call),
+                      onPressed: () => _initiateCall(context, recent, false),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    OutlinedButton(
+                      style: buttonStyle,
+                      child: const Icon(Icons.videocam),
+                      onPressed: () => _initiateCall(context, recent, true),
+                    ),
+                  ],
                 ),
                 const Divider(
                   height: 16,
@@ -67,5 +94,15 @@ class RecentScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _initiateCall(BuildContext context, Recent recent, bool video) {
+    final callBloc = context.read<CallBloc>();
+    callBloc.add(CallControlEvent.started(
+      number: recent.number,
+      displayName: recent.name,
+      video: video,
+    ));
+    context.maybePop();
   }
 }
