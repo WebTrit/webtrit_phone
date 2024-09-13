7068ca5c02d6be9e4c0a644fcbbc138a971d5250
Dmitro Serdun <d.serdun@webtrit.com>
2024-09-13 14:29:00 +0300

    refactor(webtrit_api): add custom response exception

--- packages/webtrit_api/lib/webtrit_api.dart
+++ packages/webtrit_api/lib/webtrit_api.dart
@@ -8,4 +8,4 @@ export 'src/exceptions.dart';
 export 'src/models/models.dart';
 export 'src/webtrit_api_client.dart';
 export 'src/webtrit_api_request_options.dart';
-export 'src/webtrit_api_method.dart';
+export 'src/webtrit_api_models.dart';
