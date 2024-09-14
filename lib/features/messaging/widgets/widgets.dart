19220a53204da2803ffd73f9dfcac138c63852f5
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-09-14 14:23:13 +0300

    feat: user sms numbers builder

--- lib/features/messaging/widgets/widgets.dart
+++ lib/features/messaging/widgets/widgets.dart
@@ -3,4 +3,5 @@ export 'exchange_bar.dart';
 export 'main_flavor_overlay.dart';
 export 'participant_name.dart';
 export 'contact_info_builder.dart';
+export 'user_sms_numbers_builder.dart';
 export 'shell.dart';
