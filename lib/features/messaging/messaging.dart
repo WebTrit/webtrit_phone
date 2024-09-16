6416f680c9b510100b5e0e7d398cae30e5b4f375
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-09-16 10:11:22 +0300

    refactor: sub features path name

--- lib/features/messaging/messaging.dart
+++ lib/features/messaging/messaging.dart
@@ -1,9 +1,9 @@
 export 'extensions/extensions.dart';
 export 'bloc/messaging_bloc.dart';
 export 'features/conversations/conversations.dart';
-export 'features/conversation/conversation.dart';
+export 'features/chat_conversation/conversation.dart';
 export 'features/sms_conversation/sms_conversation.dart';
-export 'features/group/group.dart';
+export 'features/group_conversation/group.dart';
 export 'features/group_builder/group_builder.dart';
 export 'cubits/cubits.dart';
 export 'view/router_page.dart';
