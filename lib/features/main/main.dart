4fb6d2bcc4c218b4879aec5effc025a638f6cdde
Dmitro Serdun <d.serdun@webtrit.com>
2024-09-16 13:22:41 +0300

    refactor: relocate main flavor to common model package

--- lib/features/main/main.dart
+++ lib/features/main/main.dart
@@ -1,6 +1,5 @@
 export 'bloc/main_bloc.dart';
 export 'extensions/extensions.dart';
-export 'models/models.dart';
 export 'view/main_screen.dart';
 export 'view/main_screen_page.dart';
 export 'widgets/widgets.dart';
