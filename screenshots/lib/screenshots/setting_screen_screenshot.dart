558474029031a4ab02c28f5c6588cc97bd9974aa
Dmitro Serdun <d.serdun@webtrit.com>
2024-09-18 14:07:52 +0300

    feat: add dynamic configuration based on environment data

--- screenshots/lib/screenshots/setting_screen_screenshot.dart
+++ screenshots/lib/screenshots/setting_screen_screenshot.dart
@@ -4,6 +4,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:webtrit_phone/features/features.dart';
+import 'package:webtrit_phone/models/models.dart';
 
 import 'package:screenshots/mocks/mocks.dart';
 
@@ -21,33 +22,78 @@ class _SettingScreenScreenshotState extends State<SettingScreenScreenshot> {
   void initState() {
     super.initState();
 
-    SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
-      if (!mounted) {
-        return;
-      }
+    SchedulerBinding.instance.addPostFrameCallback(
+      (Duration timeStamp) {
+        if (!mounted) {
+          return;
+        }
 
-      Navigator.push(
-        context,
-        PageRouteBuilder(
-          pageBuilder: (context, _, __) {
-            return MultiBlocProvider(
-              providers: [
-                BlocProvider<CallBloc>(
-                  create: (context) => MockCallBloc.settingsScreen(),
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, _, __) {
+              return MultiBlocProvider(
+                providers: [
+                  BlocProvider<CallBloc>(
+                    create: (context) => MockCallBloc.settingsScreen(),
+                  ),
+                  BlocProvider<SettingsBloc>(
+                    create: (context) => MockSettingsBloc.settingsScreen(),
+                  ),
+                ],
+                child: SettingsScreen(
+                  sections: [
+                    SettingsSection(
+                      titleL10n: 'settings_section_title_general',
+                      items: [
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_network',
+                          icon: Icons.network_check,
+                          flavor: SettingsFlavor.network,
+                        ),
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_language',
+                          icon: Icons.language,
+                          flavor: SettingsFlavor.language,
+                        ),
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_termsConditions',
+                          icon: Icons.book_outlined,
+                          flavor: SettingsFlavor.terms,
+                        ),
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_about',
+                          icon: Icons.card_travel,
+                          flavor: SettingsFlavor.about,
+                        ),
+                      ],
+                    ),
+                    SettingsSection(
+                      titleL10n: 'settings_ListViewTileTitle_toolbox',
+                      items: [
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_logRecordsConsole',
+                          icon: Icons.aod_outlined,
+                          flavor: SettingsFlavor.log,
+                        ),
+                        SettingItem(
+                          titleL10n: 'settings_ListViewTileTitle_accountDelete',
+                          icon: Icons.delete_outline,
+                          flavor: SettingsFlavor.deleteAccount,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                BlocProvider<SettingsBloc>(
-                  create: (context) => MockSettingsBloc.settingsScreen(),
-                ),
-              ],
-              child: const SettingsScreen(),
-            );
-          },
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-          fullscreenDialog: true,
-        ),
-      );
-    });
+              );
+            },
+            transitionDuration: Duration.zero,
+            reverseTransitionDuration: Duration.zero,
+            fullscreenDialog: true,
+          ),
+        );
+      },
+    );
   }
 
   @override
