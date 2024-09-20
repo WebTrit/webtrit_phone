329850e02380594a62ec42c6ea9341cca7780334
Dmitro Serdun <d.serdun@webtrit.com>
2024-09-20 14:53:46 +0300

    fix: add root route check for route match extension

--- lib/features/embedded/extensions/route_match.dart
+++ lib/features/embedded/extensions/route_match.dart
@@ -4,6 +4,10 @@ import 'package:webtrit_phone/extensions/extensions.dart';
 
 extension RouteMatchExension on RouteMatch {
   RouteMatch? findRouteWithRequiredParams(PageInfo pageInfo) {
+    if (name == pageInfo.name && args == null) {
+      return this;
+    }
+
     return children?.firstWhereOrNull(
       (child) => (child.name == pageInfo.name && child.args == null),
     );
