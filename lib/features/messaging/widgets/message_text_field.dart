ea663589906b8eb57452ba224f1ffc6cc838dbde
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-09-27 11:12:39 +0300

    fix: text field

--- lib/features/messaging/widgets/message_text_field.dart
+++ lib/features/messaging/widgets/message_text_field.dart
@@ -19,31 +19,33 @@ class MessageTextField extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       color: colorScheme.surface,
-      child: Row(
-        children: [
-          Expanded(
-            child: TextFormField(
-              controller: controller,
-              onFieldSubmitted: (_) => onSend(),
-              onChanged: (_) {},
-              decoration: InputDecoration(
-                hintText: context.l10n.messaging_MessageField_hint,
-                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                isDense: true,
-                isCollapsed: true,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(16),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: controller,
+                onFieldSubmitted: (_) => onSend(),
+                onChanged: (_) {},
+                decoration: InputDecoration(
+                  hintText: context.l10n.messaging_MessageField_hint,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  isDense: true,
+                  isCollapsed: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(width: 8),
-          GestureDetector(
-            child: Icon(Icons.send, size: 24, color: colorScheme.secondary),
-            onTap: () => onSend(),
-          ),
-        ],
+            const SizedBox(width: 8),
+            GestureDetector(
+              child: Icon(Icons.send, size: 24, color: colorScheme.secondary),
+              onTap: () => onSend(),
+            ),
+          ],
+        ),
       ),
     );
   }
