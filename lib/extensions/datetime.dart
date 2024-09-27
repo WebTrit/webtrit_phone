f89218312e99cf3195b134e9cea0c7eb5a006a3c
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-09-27 17:59:24 +0300

    fix: localtime on ui

--- lib/extensions/datetime.dart
+++ lib/extensions/datetime.dart
@@ -2,17 +2,17 @@ import 'package:intl/intl.dart';
 
 extension DateTimeFormatting on DateTime {
   /// Returns time in HH:mm format (e.g. 14:30)
-  String get toHHmm => DateFormat('HH:mm').format(this);
+  String get toHHmm => DateFormat('HH:mm').format(toLocal());
 
   /// Returns date in d MMM format (e.g. 20 Aug)
-  String get toDayOfMonth => DateFormat('d MMM').format(this);
+  String get toDayOfMonth => DateFormat('d MMM').format(toLocal());
 
   /// Returns time in HH:mm(14:30) format if the date is today, otherwise returns date in d MMM(20 Aug) format
   String get timeOrDate {
     final now = DateTime.now();
     final midnight = DateTime(now.year, now.month, now.day);
 
-    if (midnight.isBefore(this)) {
+    if (midnight.isBefore(toLocal())) {
       return toHHmm;
     } else {
       return toDayOfMonth;
