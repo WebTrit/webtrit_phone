43166c9c9f7c3e4c70c87a06ad3444fc892b1e1d
Dmitro Serdun <d.serdun@webtrit.com>
2024-10-25 02:14:03 +0300

    feat: add the possibility to use core URL from auto-provisioning

--- lib/features/autoprovision/view/autoprovision_screen_page.dart
+++ lib/features/autoprovision/view/autoprovision_screen_page.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:webtrit_phone/blocs/blocs.dart';
+import 'package:webtrit_phone/environment_config.dart';
 import 'package:webtrit_phone/features/features.dart';
 
 @RoutePage()
@@ -12,9 +13,12 @@ class AutoprovisionScreenPage extends StatelessWidget {
   const AutoprovisionScreenPage({
     @QueryParam('config_token') this.configToken,
     @QueryParam('tenant_id') this.tenantId,
+    @QueryParam('core') this.coreUrl,
   });
+
   final String? configToken;
   final String? tenantId;
+  final String? coreUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +26,12 @@ class AutoprovisionScreenPage extends StatelessWidget {
     // coz value are verified by the router guard [onAutoprovisionScreenPageRouteGuardNavigation]
     final configToken = this.configToken!;
     final tenantId = this.tenantId ?? '';
+    final coreUrl = this.coreUrl ?? context.read<AppBloc>().state.coreUrl ?? EnvironmentConfig.DEMO_CORE_URL;
     final oldToken = context.read<AppBloc>().state.token;
     final oldTenant = context.read<AppBloc>().state.tenantId ?? '';
 
     final widget = BlocProvider(
-      create: (context) => AutoprovisionCubit(configToken, tenantId, oldToken, oldTenant),
+      create: (context) => AutoprovisionCubit(configToken, tenantId, oldToken, oldTenant, coreUrl),
       child: const AutoprovisionScreen(),
     );
 
