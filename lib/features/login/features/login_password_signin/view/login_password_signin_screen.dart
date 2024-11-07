45258c681d0f58043a1d432ebc77b246be6ec2b5
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-11-07 09:26:47 +0200

    fix: password field autofill

--- lib/features/login/features/login_password_signin/view/login_password_signin_screen.dart
+++ lib/features/login/features/login_password_signin/view/login_password_signin_screen.dart
@@ -22,78 +22,82 @@ class LoginPasswordSigninScreen extends StatelessWidget {
 
         return Padding(
           padding: const EdgeInsets.fromLTRB(kInset, kInset / 2, kInset, kInset),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              if (passwordSigninPreDescriptionText.isNotEmpty) ...[
-                Description(
-                  text: passwordSigninPreDescriptionText,
-                ),
-                const SizedBox(height: kInset / 2),
-              ],
-              TextFormField(
-                enabled: !state.processing,
-                initialValue: state.passwordSigninUserRefInput.value,
-                decoration: InputDecoration(
-                  labelText: context.l10n.login_TextFieldLabelText_passwordSigninUserRef,
-                  helperText: '', // reserve space for validator message
-                  errorText: state.passwordSigninUserRefInput.displayError?.l10n(context),
-                  errorMaxLines: 3,
-                ),
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [
-                  AutofillHints.email,
-                  AutofillHints.telephoneNumber,
-                ],
-                onChanged: context.read<LoginCubit>().passwordSigninUserRefInputChanged,
-                onFieldSubmitted: !state.passwordSigninUserRefInput.isValid ? null : (_) => _onSubmitted(context),
-              ),
-              TextFormField(
-                enabled: !state.processing,
-                initialValue: state.passwordSigninPasswordInput.value,
-                decoration: InputDecoration(
-                  labelText: context.l10n.login_TextFieldLabelText_passwordSigninPassword,
-                  helperText: '', // reserve space for validator message
-                  errorText: state.passwordSigninPasswordInput.displayError?.l10n(context),
-                  errorMaxLines: 3,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      state.passwordSigninPasswordInputObscureText ? Icons.visibility : Icons.visibility_off,
-                      size: 24,
-                    ),
-                    onPressed: state.processing
-                        ? null
-                        : context.read<LoginCubit>().passwordSigninPasswordInputObscureTextToggled,
+          child: AutofillGroup(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                if (passwordSigninPreDescriptionText.isNotEmpty) ...[
+                  Description(
+                    text: passwordSigninPreDescriptionText,
                   ),
+                  const SizedBox(height: kInset / 2),
+                ],
+                TextFormField(
+                  enabled: !state.processing,
+                  initialValue: state.passwordSigninUserRefInput.value,
+                  decoration: InputDecoration(
+                    labelText: context.l10n.login_TextFieldLabelText_passwordSigninUserRef,
+                    helperText: '', // reserve space for validator message
+                    errorText: state.passwordSigninUserRefInput.displayError?.l10n(context),
+                    errorMaxLines: 3,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  autofillHints: const [
+                    AutofillHints.email,
+                    AutofillHints.telephoneNumber,
+                  ],
+                  onChanged: context.read<LoginCubit>().passwordSigninUserRefInputChanged,
+                  onFieldSubmitted: !state.passwordSigninUserRefInput.isValid ? null : (_) => _onSubmitted(context),
                 ),
-                obscureText: state.passwordSigninPasswordInputObscureText,
-                onChanged: context.read<LoginCubit>().passwordSigninPasswordInputChanged,
-                onFieldSubmitted: !state.passwordSigninPasswordInput.isValid ? null : (_) => _onSubmitted(context),
-              ),
-              if (passwordSigninPostDescriptionText.isNotEmpty) ...[
-                const SizedBox(height: kInset / 8),
-                Description(
-                  text: passwordSigninPostDescriptionText,
-                ),
-              ],
-              const Spacer(),
-              const SizedBox(height: kInset),
-              ElevatedButton(
-                onPressed: state.processing ||
-                        !state.passwordSigninUserRefInput.isValid ||
-                        !state.passwordSigninPasswordInput.isValid
-                    ? null
-                    : () => _onSubmitted(context),
-                style: elevatedButtonStyles?.primary,
-                child: !state.processing
-                    ? Text(context.l10n.login_Button_passwordSigninProceed)
-                    : SizedCircularProgressIndicator(
-                        size: 16,
-                        strokeWidth: 2,
-                        color: elevatedButtonStyles?.primary?.foregroundColor?.resolve({}),
+                TextFormField(
+                  enabled: !state.processing,
+                  initialValue: state.passwordSigninPasswordInput.value,
+                  decoration: InputDecoration(
+                    labelText: context.l10n.login_TextFieldLabelText_passwordSigninPassword,
+                    helperText: '', // reserve space for validator message
+                    errorText: state.passwordSigninPasswordInput.displayError?.l10n(context),
+                    errorMaxLines: 3,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        state.passwordSigninPasswordInputObscureText ? Icons.visibility : Icons.visibility_off,
+                        size: 24,
                       ),
-              )
-            ],
+                      onPressed: state.processing
+                          ? null
+                          : context.read<LoginCubit>().passwordSigninPasswordInputObscureTextToggled,
+                    ),
+                  ),
+                  obscureText: state.passwordSigninPasswordInputObscureText,
+                  keyboardType: TextInputType.visiblePassword,
+                  autofillHints: const [AutofillHints.password],
+                  onChanged: context.read<LoginCubit>().passwordSigninPasswordInputChanged,
+                  onFieldSubmitted: !state.passwordSigninPasswordInput.isValid ? null : (_) => _onSubmitted(context),
+                ),
+                if (passwordSigninPostDescriptionText.isNotEmpty) ...[
+                  const SizedBox(height: kInset / 8),
+                  Description(
+                    text: passwordSigninPostDescriptionText,
+                  ),
+                ],
+                const Spacer(),
+                const SizedBox(height: kInset),
+                ElevatedButton(
+                  onPressed: state.processing ||
+                          !state.passwordSigninUserRefInput.isValid ||
+                          !state.passwordSigninPasswordInput.isValid
+                      ? null
+                      : () => _onSubmitted(context),
+                  style: elevatedButtonStyles?.primary,
+                  child: !state.processing
+                      ? Text(context.l10n.login_Button_passwordSigninProceed)
+                      : SizedCircularProgressIndicator(
+                          size: 16,
+                          strokeWidth: 2,
+                          color: elevatedButtonStyles?.primary?.foregroundColor?.resolve({}),
+                        ),
+                )
+              ],
+            ),
           ),
         );
       },
