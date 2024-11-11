829bfecc57d52e0634fccd09e06b05775f95a718
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-11-11 13:12:49 +0200

    fix: current nav bar position on user navigation

--- lib/features/main/view/main_screen.dart
+++ lib/features/main/view/main_screen.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:webtrit_phone/blocs/app/app_bloc.dart';
-import 'package:webtrit_phone/l10n/l10n.dart';
-import 'package:webtrit_phone/models/models.dart';
-import 'package:webtrit_phone/features/messaging/messaging.dart';
 
 import '../main.dart';
 
@@ -13,37 +10,15 @@ class MainScreen extends StatelessWidget {
   const MainScreen({
     Key? key,
     required this.body,
-    required this.currentTab,
-    required this.tabs,
-    this.onNavigationBarTap,
+    required this.bottomNavigationBar,
   }) : super(key: key ?? const ValueKey<String>('MainScreen'));
 
   final Widget body;
-  final BottomMenuTab currentTab;
-  final List<BottomMenuTab> tabs;
-  final ValueChanged<BottomMenuTab>? onNavigationBarTap;
+  final BottomNavigationBar bottomNavigationBar;
 
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-    final scaffold = Scaffold(
-      body: body,
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: themeData.textTheme.bodySmall,
-        unselectedLabelStyle: themeData.textTheme.bodySmall,
-        currentIndex: tabs.indexOf(currentTab),
-        items: tabs.map((tab) {
-          final flavor = tab.flavor;
-          Widget icon = Icon(tab.icon);
-          String label = context.parseL10n(tab.titleL10n);
-          if (flavor == MainFlavor.messaging) icon = MessagingFlavorOverlay(child: icon);
-          return BottomNavigationBarItem(icon: icon, label: label);
-        }).toList(),
-        onTap: (index) => onNavigationBarTap?.call(tabs[index]),
-        // items: navBarItems,
-      ),
-    );
+    final scaffold = Scaffold(body: body, bottomNavigationBar: bottomNavigationBar);
 
     return BlocListener<MainBloc, MainState>(
       listener: (context, state) async {
