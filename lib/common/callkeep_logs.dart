b365e7899b6c2258d45ad198e4544d5abfc8155c
Dmitro Serdun <d.serdun@webtrit.com>
2024-11-19 13:15:47 +0200

    feat: enhance logging with Crashlytics integration for Callkeep logs

--- lib/common/callkeep_logs.dart
+++ lib/common/callkeep_logs.dart
@@ -1,5 +1,7 @@
 import 'package:logging/logging.dart';
 
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
 import 'package:webtrit_callkeep/webtrit_callkeep.dart';
 
 class CallkeepLogs implements CallkeepLogsDelegate {
@@ -7,6 +9,36 @@ class CallkeepLogs implements CallkeepLogsDelegate {
 
   @override
   void onLog(CallkeepLogType type, String tag, String message) {
-    _logger.info('$tag $message');
+    final formattedMessage = '[$tag] $message';
+
+    switch (type) {
+      case CallkeepLogType.debug:
+        _logger.info(formattedMessage);
+      case CallkeepLogType.error:
+        _recordCrashlyticsError(formattedMessage, fatal: true);
+        _logger.severe(formattedMessage);
+      case CallkeepLogType.info:
+        _logger.info(formattedMessage);
+      case CallkeepLogType.verbose:
+        _logger.info(formattedMessage);
+      case CallkeepLogType.warn:
+        _recordCrashlyticsError(formattedMessage);
+        _logger.warning(formattedMessage);
+    }
+  }
+
+  void _recordCrashlyticsError(
+    String message, {
+    bool fatal = false,
+  }) {
+    try {
+      FirebaseCrashlytics.instance.recordError(
+        Exception(message),
+        null,
+        fatal: fatal,
+      );
+    } catch (e) {
+      _logger.warning('Failed to record error to FirebaseCrashlytics: $e');
+    }
   }
 }
