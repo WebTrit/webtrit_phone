1136d9f46aab28e0eee817862d54f96f257bd78d
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-11-22 10:04:46 +0200

    refactor: favorite full contact data retrieve

--- lib/repositories/favorites/favorites_repository.dart
+++ lib/repositories/favorites/favorites_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:webtrit_phone/data/data.dart';
 import 'package:webtrit_phone/extensions/extensions.dart';
 import 'package:webtrit_phone/models/models.dart';
+import 'package:webtrit_phone/utils/utils.dart';
 
 class FavoritesRepository {
   FavoritesRepository({
@@ -12,10 +13,14 @@ class FavoritesRepository {
   final AppDatabase _appDatabase;
 
   Stream<List<Favorite>> favorites() {
-    return _appDatabase.favoritesDao.watchFavoritesExt().map((favoritesExt) => favoritesExt.map((favoriteExt) {
-          final favoriteData = favoriteExt.favoriteData;
-          final contactPhoneData = favoriteExt.contactPhoneData;
-          final contactData = favoriteExt.contactData;
+    return _appDatabase.favoritesDao.watchFavoritesExt().map((favoritesExt) => favoritesExt.map((data) {
+          final favoriteData = data.favoriteData;
+          final contactPhoneData = data.contactPhoneData;
+          final contactData = data.contactData;
+
+          final email = data.contactEmails.firstOrNull?.address;
+          final gravatarUrl = gravatarThumbnailUrl(email);
+
           return Favorite(
             id: favoriteData.id,
             number: contactPhoneData.number,
@@ -28,6 +33,10 @@ class FavoritesRepository {
               firstName: contactData.firstName,
               lastName: contactData.lastName,
               aliasName: contactData.aliasName,
+              thumbnail: contactData.thumbnail,
+              thumbnailUrl: gravatarUrl,
+              phones: data.contactPhones.map(_toRealContactPhone).toList(),
+              emails: data.contactEmails.map(_toContactEmail).toList(),
             ),
           );
         }).toList(growable: false));
@@ -37,5 +46,22 @@ class FavoritesRepository {
     _appDatabase.favoritesDao.deleteFavorite(FavoriteDataCompanion(
       id: Value(favorite.id),
     ));
+  }
+
+  ContactPhone _toRealContactPhone(ContactPhoneData data) {
+    return ContactPhone(
+      id: data.id,
+      number: data.number,
+      label: data.label,
+      favorite: false,
+    );
+  }
+
+  ContactEmail _toContactEmail(ContactEmailData data) {
+    return ContactEmail(
+      id: data.id,
+      address: data.address,
+      label: data.label,
+    );
   }
 }
