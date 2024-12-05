5a390cb4b71dc14c08900ae935a1d124c0575567
Dmitro Serdun <d.serdun@webtrit.com>
2024-12-05 12:46:25 +0200

    chore: remove unused and outdated package

--- packages/webtrit_callkeep/ios/Classes/WebtritCallkeepPlugin.h
+++
@@ -1,6 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface WebtritCallkeepPlugin : NSObject<FlutterPlugin>
-- (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar;
-- (BOOL)isSetUp;
-@end
