2d4b923f5cb81c496481ea18bdf7aada14beed99
Dmitro Serdun <d.serdun@webtrit.com>
2024-12-05 12:48:07 +0200

    refactor: clean up project (#334)
    * build: update iOS dependencies
    
    * chore: remove unused and outdated package

--- packages/webtrit_callkeep/lib/webtrit_callkeep.dart
+++
@@ -1,7 +0,0 @@
-export 'package:uuid/uuid.dart';
-
-export 'src/callkeep.dart';
-export 'src/callkeep_delegate.dart';
-export 'src/callkeep_handle.dart';
-export 'src/callkeep_options.dart';
-export 'src/push_registry_delegate.dart';
