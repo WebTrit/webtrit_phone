33e6cbad53222d8d698ababa502ef76023d356d1
Dmitro Serdun <d.serdun@webtrit.com>
2024-12-23 02:55:04 +0200

    refactor: introduce strategy pattern for Logz.io initialization and group logging classes

---
+++ lib/common/logging/logging.dart
@@ -0,0 +1,6 @@
+export 'callkeep_logs.dart';
+export 'filtered_logz_io_appender.dart';
+export 'logzio_logging_service.dart';
+export 'remote_formatter.dart';
+export 'remote_log_filter.dart';
+export 'remote_logging_service.dart';
