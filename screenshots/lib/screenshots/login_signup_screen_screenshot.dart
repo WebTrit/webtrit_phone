bc61ba3bf99edf0a9e59020a357a6a4889a610f9
Dmitro Serdun <d.serdun@webtrit.com>
2025-01-27 07:53:35 +0200

    feat(screenshots): update codebase to align with the new app scheme

--- screenshots/lib/screenshots/login_signup_screen_screenshot.dart
+++ screenshots/lib/screenshots/login_signup_screen_screenshot.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:webtrit_phone/data/data.dart';
+import 'package:webtrit_phone/extensions/extensions.dart';
 import 'package:webtrit_phone/features/features.dart';
+import 'package:webtrit_phone/models/models.dart';
 
 import 'package:screenshots/mocks/mocks.dart';
 
@@ -11,12 +14,22 @@ class LoginSignUpScreenshot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sections = context
+        .watch<FeatureAccess?>()
+        ?.loginFeature
+        .actions
+        .firstWhereOrNull((element) => element.flavor == LoginFlavor.embedded);
+
+    final embedded = sections as LoginEmbeddedModeButton?;
+
     return BlocProvider<LoginCubit>(
-      create: (context) => MockLoginCubit.loginSwitchScreen(),
-      child: const LoginSwitchScreen(
-        body: LoginSignupRequestScreen(),
+      create: (context) => MockLoginCubit.loginSwitchScreen(embedded: embedded.customLoginFeature),
+      child: LoginSwitchScreen(
+        body: LoginSignupEmbeddedRequestScreen(
+          initialUrl: embedded!.customLoginFeature.resource,
+        ),
         currentLoginType: LoginType.signup,
-        supportedLoginTypes: [
+        supportedLoginTypes: const [
           LoginType.otpSignin,
           LoginType.passwordSignin,
           LoginType.signup,
