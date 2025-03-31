fcdec076982abf3a7d43ff6b4dfc2564fe5ad85c
Dmitro Serdun <d.serdun@webtrit.com>
2025-03-31 14:56:50 +0300

    refactor: remove pre-bootstrap env and function for setting web metadata description (now handled by CLI)

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,10 @@ import 'package:webtrit_phone/app/app.dart';
 import 'package:webtrit_phone/bootstrap.dart';
 import 'package:webtrit_phone/data/data.dart';
 import 'package:webtrit_phone/environment_config.dart';
-import 'package:webtrit_phone/pre_bootstrap/pre_bootstrap.dart';
 import 'package:webtrit_phone/repositories/repositories.dart';
 import 'package:webtrit_phone/utils/utils.dart';
 
 void main() {
-  preBootstrap();
-
   bootstrap(() async {
     final rootLogger = Logger.root;
     final logRecordsRepository = LogRecordsRepository()..attachToLogger(rootLogger);
--- lib/pre_bootstrap/pre_bootstrap.dart
+++
@@ -1,2 +0,0 @@
-export 'stub.dart' // stub implementation with no operation
-    if (dart.library.js) 'web.dart'; // web implementation
