77594ca0eae8eb60591cec2898b4c73301605321
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-10 09:00:15 +0300

    chore: remove unused imports and add missing logs

--- lib/app/router/deeplinks.dart
+++ lib/app/router/deeplinks.dart
@@ -2,8 +2,6 @@ import 'package:auto_route/auto_route.dart';
 
 import 'package:logging/logging.dart';
 
-import 'package:webtrit_callkeep/webtrit_callkeep.dart';
-
 import 'package:webtrit_phone/app/router/app_router.dart';
 import 'package:webtrit_phone/extensions/extensions.dart';
 import 'package:webtrit_phone/features/undefined/undefined.dart';
@@ -35,7 +33,11 @@ class HandleAutoprovision implements DeepLinkHandler {
   final PlatformDeepLink deepLink;
 
   @override
-  DeepLink? handle() => _isAutoprovision ? deepLink : null;
+  DeepLink? handle() {
+    _logger.fine('HandleAutoprovision: ${deepLink.path}');
+
+    return _isAutoprovision ? deepLink : null;
+  }
 
   bool get _isAutoprovision => deepLink.path.startsWith(kAutoprovisionRout);
 }
@@ -48,6 +50,8 @@ class HandleNotDefinedPath implements DeepLinkHandler {
 
   @override
   DeepLink? handle() {
+    _logger.fine('HandleNotDefinedPath: ${deepLink.path}');
+
     if (!_isDeeplinkExternal) return null;
     if (!_isInitial && _isInvalidScreenActive) return DeepLink.none;
 
