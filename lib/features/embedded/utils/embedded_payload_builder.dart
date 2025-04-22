be6695726f73af5655e9897ca57a69a1080e8852
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-22 19:00:50 +0300

    feat: add embedded payload builder for constructing injected data

---
+++ lib/features/embedded/utils/embedded_payload_builder.dart
@@ -0,0 +1,31 @@
+import 'package:webtrit_phone/data/data.dart';
+import 'package:webtrit_phone/models/models.dart';
+
+/// Builds the WebView payload on demand
+class EmbeddedPayloadBuilder {
+  EmbeddedPayloadBuilder(this._secureStorage);
+
+  final SecureStorage _secureStorage;
+
+  /// Builds a payload Map based on requested fields
+  Map<String, dynamic> build(List<EmbeddedPayloadData> fields) {
+    final Map<String, dynamic> payload = {};
+
+    for (final field in fields) {
+      switch (field) {
+        case EmbeddedPayloadData.userId:
+          payload['userId'] = _secureStorage.readUserId();
+          break;
+        case EmbeddedPayloadData.coreToken:
+          payload['coreToken'] = _secureStorage.readToken();
+          break;
+        case EmbeddedPayloadData.externalPageToken:
+          final token = _secureStorage.readExternalPageToken();
+          if (token != null) payload['externalPageToken'] = token;
+          break;
+      }
+    }
+
+    return payload;
+  }
+}
