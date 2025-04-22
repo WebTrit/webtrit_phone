d428361c87469f7bcc94dfc28f186c95d37ef2da
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-22 16:36:17 +0300

    feat: add embedded feature to FeatureAccess

--- lib/models/feature_access/embedded_data.dart
+++ lib/models/feature_access/embedded_data.dart
@@ -1,10 +1,12 @@
 class EmbeddedData {
   EmbeddedData({
+    required this.id,
     required this.uri,
     this.titleL10n,
   });
 
   /// The URI representing either a local asset file path or a remote URL.
+  final int id;
   final Uri uri;
 
   /// The key to use to look up the localized title.
