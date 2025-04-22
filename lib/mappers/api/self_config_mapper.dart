8f0fdfcd40faca7bf508a2023a45a07c18a212cc
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-22 18:47:14 +0300

    feat: add mapper for expiring token model

--- lib/mappers/api/self_config_mapper.dart
+++ lib/mappers/api/self_config_mapper.dart
@@ -1,9 +1,14 @@
 import 'package:webtrit_api/webtrit_api.dart' as api;
 
+import 'package:webtrit_phone/models/models.dart';
 import 'package:webtrit_phone/models/self_config.dart';
 
 mixin SelfConfigApiMapper {
   SelfConfig selfConfigFromApi(api.SelfConfigResponse data) {
     return SelfConfig.supported(url: data.url, expiresAt: data.expiresAt);
+  }
+
+  ExpiringToken externalPageTokenFromApi(api.ExternalPageAccessToken data) {
+    return ExpiringToken(data.token, data.expiresAt);
   }
 }
