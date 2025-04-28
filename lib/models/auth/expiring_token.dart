9037aa876aa421a5296e8c6321b7bf98826a36b1
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-28 09:35:02 +0300

    fix: add expiration buffer when validating token validity

--- lib/models/auth/expiring_token.dart
+++ lib/models/auth/expiring_token.dart
@@ -5,5 +5,7 @@ class ExternalPageToken {
   final String refreshToken;
   final DateTime expiration;
 
-  bool get isValid => DateTime.now().isBefore(expiration);
+  static const Duration _expirationBuffer = Duration(minutes: 5);
+
+  bool get isValid => DateTime.now().isBefore(expiration.subtract(_expirationBuffer));
 }
