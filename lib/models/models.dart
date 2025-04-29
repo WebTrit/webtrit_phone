209f23aef4cebb7304a1beb3e9cabad3a57da606
Dmitro Serdun <d.serdun@webtrit.com>
2025-04-29 08:57:35 +0300

    feat: add voicemail models

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -51,3 +51,4 @@ export 'sms_outbox_message_entry.dart';
 export 'sms_outbox_read_cursor_entry.dart';
 export 'system_info/system_info.dart';
 export 'video_capturing_settings.dart';
+export 'voicemail/voicemail.dart';
---
+++ lib/models/voicemail/user_voicemail.dart
@@ -0,0 +1,25 @@
+class Voicemail {
+  Voicemail(
+    this.id,
+    this.date,
+    this.duration,
+    this.sender,
+    this.displaySender,
+    this.receiver,
+    this.seen,
+    this.size,
+    this.type,
+    this.url,
+  );
+
+  final int id;
+  final String date;
+  final double duration;
+  final String sender;
+  final String displaySender;
+  final String receiver;
+  final bool seen;
+  final int size;
+  final String type;
+  final String? url;
+}
