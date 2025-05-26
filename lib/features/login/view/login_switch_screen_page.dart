ee862dcb16770ecdb284ed41ee7054aa246b437b
Dmitro Serdun <d.serdun@webtrit.com>
2025-05-26 15:16:41 +0300

    fix: make app bar optional for embedded pages used as launch screens (#544)
    * refactor: make app bar optional for cases like embedded pages with controlled navigation
    
    * refactor: remove isLogoVisible flag and make header customizable via constructor

--- lib/features/login/view/login_switch_screen_page.dart
+++ lib/features/login/view/login_switch_screen_page.dart
@@ -4,48 +4,55 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:webtrit_phone/features/features.dart';
+import 'package:webtrit_phone/widgets/widgets.dart';
+import 'package:webtrit_phone/app/constants.dart';
 
 @RoutePage()
 class LoginSwitchScreenPage extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
   const LoginSwitchScreenPage({
-    this.forceLoginTypes = const [],
+    this.forceLoginTypes,
     this.isLogoVisible = true,
+    this.isAppBarVisible = true,
   });
 
-  final List<LoginType> forceLoginTypes;
+  final List<LoginType>? forceLoginTypes;
   final bool isLogoVisible;
+  final bool isAppBarVisible;
 
   @override
   Widget build(BuildContext context) {
-    if (forceLoginTypes.isNotEmpty) {
-      return _buildTabs(forceLoginTypes);
-    }
-
     return BlocBuilder<LoginCubit, LoginState>(
       buildWhen: (previous, current) =>
-      whenLoginSwitchScreenPageActive(current) && previous.supportedLoginTypes != current.supportedLoginTypes,
+          whenLoginSwitchScreenPageActive(current) && previous.supportedLoginTypes != current.supportedLoginTypes,
       builder: (context, state) {
-        final supportedLoginTypes = state.supportedLoginTypes!;
-        return _buildTabs(supportedLoginTypes);
-      },
-    );
-  }
-
-  Widget _buildTabs(List<LoginType> types) {
-    return AutoTabsRouter(
-      routes: types.map((e) => e.toPageRouteInfo()).toList(growable: false),
-      duration: Duration.zero,
-      builder: (context, child) {
-        final tabsRouter = AutoTabsRouter.of(context);
-        return LoginSwitchScreen(
-          body: child,
-          currentLoginType: types[tabsRouter.activeIndex],
-          supportedLoginTypes: types,
-          onLoginTypeChanged: (loginType) {
-            tabsRouter.setActiveIndex(types.indexOf(loginType));
+        final supportedLoginTypes = forceLoginTypes ?? state.supportedLoginTypes!;
+        return AutoTabsRouter(
+          routes: supportedLoginTypes.map((e) => e.toPageRouteInfo()).toList(growable: false),
+          duration: Duration.zero,
+          builder: (context, child) {
+            final tabsRouter = AutoTabsRouter.of(context);
+            return LoginSwitchScreen(
+              appBar: isAppBarVisible
+                  ? AppBar(
+                      leading: ExtBackButton(disabled: state.processing),
+                      backgroundColor: Colors.transparent,
+                    )
+                  : null,
+              header: isLogoVisible
+                  ? const Column(
+                      children: [
+                        OnboardingLogo(),
+                        SizedBox(height: kInset),
+                      ],
+                    )
+                  : null,
+              body: child,
+              currentLoginType: supportedLoginTypes[tabsRouter.activeIndex],
+              supportedLoginTypes: supportedLoginTypes,
+              onLoginTypeChanged: (loginType) => tabsRouter.setActiveIndex(supportedLoginTypes.indexOf(loginType)),
+            );
           },
-          isLogoVisible: isLogoVisible,
         );
       },
     );
