a4d2cfecf8d079643d7dd2d012f879ebe41e21ba
Vladislav Komelkov <v.komelkov@webtrit.com>
2025-06-02 16:41:27 +0300

    feat: workmanager ios setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 import flutter_local_notifications
+import workmanager
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,14 +10,23 @@ import flutter_local_notifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-           GeneratedPluginRegistrant.register(with: registry)
-       }
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
 
-       if #available(iOS 10.0, *) {
-         UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-       }
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter
+        .current().delegate = self as UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "systemNotificationsTask-id", frequency: NSNumber(value: 1 * 60))
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 }
