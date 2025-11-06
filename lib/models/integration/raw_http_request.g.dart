b3e71b7b5811beaf502ee9a9ae51d3f8b6d40be1
Dmytro Serdun <d.serdun@webtrit.com>
2025-11-06 08:43:10 +0200

    chore: regenerate *.g.dart and *.gen.dart files after updating format width to 120

--- lib/models/integration/raw_http_request.g.dart
+++ lib/models/integration/raw_http_request.g.dart
@@ -6,16 +6,20 @@ part of 'raw_http_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-RawHttpRequest _$RawHttpRequestFromJson(Map<String, dynamic> json) => RawHttpRequest(
-  method: json['method'] as String,
-  url: json['url'] as String,
-  headers: (json['headers'] as Map<String, dynamic>?)?.map((k, e) => MapEntry(k, e as String)),
-  data: json['data'] as Map<String, dynamic>?,
-);
+RawHttpRequest _$RawHttpRequestFromJson(Map<String, dynamic> json) =>
+    RawHttpRequest(
+      method: json['method'] as String,
+      url: json['url'] as String,
+      headers: (json['headers'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, e as String),
+      ),
+      data: json['data'] as Map<String, dynamic>?,
+    );
 
-Map<String, dynamic> _$RawHttpRequestToJson(RawHttpRequest instance) => <String, dynamic>{
-  'method': instance.method,
-  'url': instance.url,
-  'headers': instance.headers,
-  'data': instance.data,
-};
+Map<String, dynamic> _$RawHttpRequestToJson(RawHttpRequest instance) =>
+    <String, dynamic>{
+      'method': instance.method,
+      'url': instance.url,
+      'headers': instance.headers,
+      'data': instance.data,
+    };
