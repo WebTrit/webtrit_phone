404c2e3407bf20ba583b8a22c056b45002b00a5b
Dmitro Serdun <d.serdun@webtrit.com>
2025-12-15 18:30:28 +0200

    fix: add missed mock MicrophoneStatusBloc for screenshot builds (#806)

--- screenshots/lib/mocks/mocks.dart
+++ screenshots/lib/mocks/mocks.dart
@@ -13,6 +13,7 @@ export 'mock_keypad_cubit.dart';
 export 'mock_login_cubit.dart';
 export 'mock_main_bloc.dart';
 export 'mock_messaging_bloc.dart';
+export 'mock_microphone_status_bloc.dart';
 export 'mock_recents_bloc.dart';
 export 'mock_register_status_cubit.dart';
 export 'mock_session_status_cubit.dart';
