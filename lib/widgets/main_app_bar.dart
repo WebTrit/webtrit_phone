bb519cd69d415e15906d9ca4c8b89db88ed38388
Dmytro Serdun <d.serdun@webtrit.com>
2025-12-22 21:56:56 +0200

    feat: allow passing backgroundColor and elevation to MainAppBar

--- lib/widgets/main_app_bar.dart
+++ lib/widgets/main_app_bar.dart
@@ -13,71 +13,77 @@ import 'package:webtrit_phone/utils/utils.dart';
 import 'package:webtrit_phone/widgets/widgets.dart';
 
 class MainAppBar extends AppBar {
-  MainAppBar({super.key, super.title, super.bottom, required BuildContext context})
-    : super(
-        centerTitle: false,
-        actions: [
-          if (AppBarParams.of(context).pullableCalls.isNotEmpty)
-            CallPullBadge(pullableCalls: AppBarParams.of(context).pullableCalls),
-          if (AppBarParams.of(context).systemNotificationsEnabled) SystemNotificationsBadge(),
-          BlocBuilder<SessionStatusCubit, SessionStatusState>(
-            builder: (context, sessionState) {
-              return Ink(
-                decoration: ShapeDecoration(
-                  shape: CircleBorder(side: BorderSide(color: sessionState.status.color(context))),
-                ),
-                child: BlocBuilder<UserInfoCubit, UserInfoState>(
-                  builder: (context, userinfoState) {
-                    final info = userinfoState.userInfo;
-                    return IconButton(
-                      key: mainAppBarKey,
-                      constraints: const BoxConstraints.tightFor(
-                        width: kMinInteractiveDimension,
-                        height: kMinInteractiveDimension,
-                      ),
-                      padding: const EdgeInsets.all(2),
-                      icon: Stack(
-                        clipBehavior: Clip.none,
-                        children: <Widget>[
-                          LeadingAvatar(
-                            username: info?.name ?? info?.numbers.main,
-                            thumbnailUrl: gravatarThumbnailUrl(info?.email),
-                            radius: kMinInteractiveDimension / 2,
-                            showLoading: true,
-                          ),
-                          BlocBuilder<MicrophoneStatusBloc, MicrophoneStatusState>(
-                            builder: (context, microphoneStatusState) {
-                              return Visibility(
-                                visible:
-                                    microphoneStatusState.microphonePermissionGranted != null &&
-                                    !microphoneStatusState.microphonePermissionGranted!,
-                                child: Positioned(
-                                  right: -8,
-                                  top: -2,
-                                  child: Container(
-                                    padding: EdgeInsets.all(3),
-                                    decoration: BoxDecoration(
-                                      color: Theme.of(context).colorScheme.error,
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Icon(Icons.mic_off, color: Colors.white, size: 14),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      onPressed: () {
-                        context.router.navigate(const SettingsRouterPageRoute());
-                      },
-                    );
-                  },
-                ),
-              );
-            },
-          ),
-          const SizedBox(width: NavigationToolbar.kMiddleSpacing),
-        ],
-      );
+  MainAppBar({
+    super.key,
+    super.title,
+    super.bottom,
+    required BuildContext context,
+    super.backgroundColor,
+    super.elevation,
+  }) : super(
+         centerTitle: false,
+         actions: [
+           if (AppBarParams.of(context).pullableCalls.isNotEmpty)
+             CallPullBadge(pullableCalls: AppBarParams.of(context).pullableCalls),
+           if (AppBarParams.of(context).systemNotificationsEnabled) SystemNotificationsBadge(),
+           BlocBuilder<SessionStatusCubit, SessionStatusState>(
+             builder: (context, sessionState) {
+               return Ink(
+                 decoration: ShapeDecoration(
+                   shape: CircleBorder(side: BorderSide(color: sessionState.status.color(context))),
+                 ),
+                 child: BlocBuilder<UserInfoCubit, UserInfoState>(
+                   builder: (context, userinfoState) {
+                     final info = userinfoState.userInfo;
+                     return IconButton(
+                       key: mainAppBarKey,
+                       constraints: const BoxConstraints.tightFor(
+                         width: kMinInteractiveDimension,
+                         height: kMinInteractiveDimension,
+                       ),
+                       padding: const EdgeInsets.all(2),
+                       icon: Stack(
+                         clipBehavior: Clip.none,
+                         children: <Widget>[
+                           LeadingAvatar(
+                             username: info?.name ?? info?.numbers.main,
+                             thumbnailUrl: gravatarThumbnailUrl(info?.email),
+                             radius: kMinInteractiveDimension / 2,
+                             showLoading: true,
+                           ),
+                           BlocBuilder<MicrophoneStatusBloc, MicrophoneStatusState>(
+                             builder: (context, microphoneStatusState) {
+                               return Visibility(
+                                 visible:
+                                     microphoneStatusState.microphonePermissionGranted != null &&
+                                     !microphoneStatusState.microphonePermissionGranted!,
+                                 child: Positioned(
+                                   right: -8,
+                                   top: -2,
+                                   child: Container(
+                                     padding: EdgeInsets.all(3),
+                                     decoration: BoxDecoration(
+                                       color: Theme.of(context).colorScheme.error,
+                                       shape: BoxShape.circle,
+                                     ),
+                                     child: Icon(Icons.mic_off, color: Colors.white, size: 14),
+                                   ),
+                                 ),
+                               );
+                             },
+                           ),
+                         ],
+                       ),
+                       onPressed: () {
+                         context.router.navigate(const SettingsRouterPageRoute());
+                       },
+                     );
+                   },
+                 ),
+               );
+             },
+           ),
+           const SizedBox(width: NavigationToolbar.kMiddleSpacing),
+         ],
+       );
 }
