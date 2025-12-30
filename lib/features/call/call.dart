c16c1434bd678251cad18b009ac0c5b2c558c45c
Dmytro Serdun <d.serdun@webtrit.com>
2025-12-30 23:03:00 +0200

    refactor: export extensions from call barrel file

--- lib/features/call/call.dart
+++ lib/features/call/call.dart
@@ -1,5 +1,6 @@
 export 'bloc/call_bloc.dart';
 export 'controllers/controllers.dart';
+export 'extensions/extensions.dart';
 export 'models/models.dart';
 export 'services/services.dart';
 export 'utils/utils.dart';
