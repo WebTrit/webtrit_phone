0cdad215d12e665dbcda3c259421797c00701172
Dmytro Serdun <d.serdun@webtrit.com>
2026-01-12 17:47:56 +0200

    refactor: extract preferred orientation lists into constants

--- lib/utils/orientations.dart
+++ lib/utils/orientations.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/services.dart';
 
+/// All supported device orientations.
+const _allOrientations = [
+  DeviceOrientation.portraitUp,
+  DeviceOrientation.portraitDown,
+  DeviceOrientation.landscapeLeft,
+  DeviceOrientation.landscapeRight,
+];
+
+/// Portrait-only orientations.
+const _portraitOrientations = [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown];
+
 Future<void> setPortraitPreferredOrientations() {
-  return SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  return SystemChrome.setPreferredOrientations(_portraitOrientations);
 }
 
 Future<void> setAllPreferredOrientations() {
-  return SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
+  return SystemChrome.setPreferredOrientations(_allOrientations);
 }
