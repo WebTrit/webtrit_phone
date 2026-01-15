d1c150485b18e8d71920fcf2d6c482bc67bd2383
Dmytro Serdun <d.serdun@webtrit.com>
2026-01-15 14:45:45 +0200

    refactor: make DefaultPeerConnectionFactory configurable and reuse default ICE config

--- lib/features/call/utils/peer_connection_factory.dart
+++ lib/features/call/utils/peer_connection_factory.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
-/// Abstract factory to create RTCPeerConnection instances.
+/// Abstract factory to create [RTCPeerConnection] instances.
 abstract interface class PeerConnectionFactory {
   Future<RTCPeerConnection> create([
     Map<String, dynamic> configuration = const {},
@@ -10,22 +10,25 @@ abstract interface class PeerConnectionFactory {
 
 /// Default implementation using the actual Flutter WebRTC plugin.
 class DefaultPeerConnectionFactory implements PeerConnectionFactory {
-  const DefaultPeerConnectionFactory();
+  static const Map<String, dynamic> _defaultIceConfiguration = {
+    'iceServers': [
+      {'url': 'stun:stun.l.google.com:19302'},
+    ],
+  };
+
+  final Map<String, dynamic> _defaultConfiguration;
+
+  const DefaultPeerConnectionFactory({Map<String, dynamic> defaultConfiguration = _defaultIceConfiguration})
+    : _defaultConfiguration = defaultConfiguration;
 
   @override
   Future<RTCPeerConnection> create([
     Map<String, dynamic> configuration = const {},
     Map<String, dynamic> constraints = const {},
   ]) {
-    final defaultConfiguration = {
-      'iceServers': [
-        {'url': 'stun:stun.l.google.com:19302'},
-      ],
-    };
-
     // Use default configuration only if the provided one is empty.
     // This allows passing specific configurations when needed.
-    final effectiveConfiguration = configuration.isEmpty ? defaultConfiguration : configuration;
+    final effectiveConfiguration = configuration.isEmpty ? _defaultConfiguration : configuration;
 
     return createPeerConnection(effectiveConfiguration, constraints);
   }
