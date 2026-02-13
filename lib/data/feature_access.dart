bdc541d3962cb1b7a841004d6f55847713eabf7f
Dmytro Serdun <d.serdun@webtrit.com>
2026-02-13 09:56:27 +0200

    fix(data): add monitoringConfig to FeatureAccess Equatable props

--- lib/data/feature_access.dart
+++ lib/data/feature_access.dart
@@ -152,6 +152,7 @@ class FeatureAccess extends Equatable {
     termsConfig,
     systemNotificationsConfig,
     sipPresenceConfig,
+    monitoringConfig,
     supportedConfig,
     coreSupport,
     overrides,
