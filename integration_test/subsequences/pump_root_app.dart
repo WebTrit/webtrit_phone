3047cf34b08f56c7a9a0a3ed5b9a7b15fb269cde
Vladislav Komelkov <v.komelkov@webtrit.com>
2026-03-30 09:22:45 +0300

    fix: remove log file from standart test

--- integration_test/subsequences/pump_root_app.dart
+++ integration_test/subsequences/pump_root_app.dart
@@ -2,14 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:webtrit_phone/common/common.dart';
 import 'package:webtrit_phone/main.dart';
-import 'package:webtrit_phone/utils/utils.dart';
 
 Future<void> pumpRootApp(InstanceRegistry instanceRegistry, WidgetTester tester) async {
-  final appDocDir = await getApplicationDocumentsPath();
-  final String baseLogDirectoryPath = '$appDocDir/logs';
-  final String baseLogFilePath = '$baseLogDirectoryPath/app_logs.log';
-
-  RootApp rootApp = RootApp(instanceRegistry: instanceRegistry, baseLogFilePath: baseLogFilePath);
+  RootApp rootApp = RootApp(instanceRegistry: instanceRegistry);
   await tester.pumpWidget(rootApp);
   await tester.pumpAndSettle();
 }
