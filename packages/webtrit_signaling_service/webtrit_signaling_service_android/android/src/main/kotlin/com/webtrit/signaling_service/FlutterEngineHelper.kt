302606f4a0bcb9c4385a01e4796a35bba28782ea
Dmitro Serdun <d.serdun@webtrit.com>
2026-04-21 19:27:14 +0300

    fix(signaling): use callbackName instead of non-existent callbackEntrypointFunctionName (#1188)

--- packages/webtrit_signaling_service/webtrit_signaling_service_android/android/src/main/kotlin/com/webtrit/signaling_service/FlutterEngineHelper.kt
+++ packages/webtrit_signaling_service/webtrit_signaling_service_android/android/src/main/kotlin/com/webtrit/signaling_service/FlutterEngineHelper.kt
@@ -66,8 +66,7 @@ class FlutterEngineHelper(
             hasInvalidHandle = false
             Log.d(TAG, "executeDartCallback: handle=$callbackHandle " +
                 "library=${callbackInformation.callbackLibraryPath} " +
-                "function=${callbackInformation.callbackEntrypointFunctionName} " +
-                "class=${callbackInformation.callbackEntrypointFunctionClass}")
+                "function=${callbackInformation.callbackName}")
             // automaticallyRegisterPlugins=false: prevents GeneratedPluginRegistrant from
             // registering all app plugins (AudioSessionPlugin, FlutterWebRTCPlugin, etc.)
             // on this background FGS engine. On Android 16 REMOTE_MESSAGING services,
