7e6c79261c0729cb7076cfe7645b8a20ff62d072
Vladislav Komelkov <v.komelkov@webtrit.com>
2026-05-11 14:59:30 +0300

    fix: keypad screen optimizations

--- lib/features/keypad/cubit/keypad_cubit.dart
+++ lib/features/keypad/cubit/keypad_cubit.dart
@@ -3,29 +3,28 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:webtrit_phone/models/models.dart';
 import 'package:webtrit_phone/repositories/repositories.dart';
+import 'package:webtrit_phone/utils/debounce.dart';
 
 part 'keypad_state.dart';
 
 part 'keypad_cubit.freezed.dart';
 
 class KeypadCubit extends Cubit<KeypadState> {
+  KeypadCubit(this._contactsRepository) : super(KeypadState());
+
   final ContactsRepository _contactsRepository;
+  final _contactDebounce = Debounce();
 
-  KeypadCubit(this._contactsRepository) : super(const KeypadState());
-
-  /// Attempts to retrieve and update the contact matching the provided [number].
+  /// Sets current value and attempts to retrieve and update the contact matching the provided [number].
   ///
   /// Trims leading and trailing whitespace from [number].
   /// - If the cleaned number is empty, the contact state is reset to `null`.
   /// - Otherwise, delegates to [_fetchContact] to perform the asynchronous lookup.
-  Future<void> getContactByPhoneNumber(String number) async {
+  Future<void> setValue(String number) async {
     final cleanedNumber = number.trim();
 
-    if (cleanedNumber.isEmpty) {
-      emit(state.copyWith(contact: null));
-    } else {
-      await _fetchContact(cleanedNumber);
-    }
+    emit(state.copyWith(contact: null, value: number));
+    if (cleanedNumber.isNotEmpty) _contactDebounce.schedule(() => _fetchContact(cleanedNumber));
   }
 
   /// Asynchronously fetches the contact from the repository.
@@ -33,13 +32,15 @@ class KeypadCubit extends Cubit<KeypadState> {
   /// - On success: updates the state with the retrieved contact.
   /// - On error: resets the contact state to `null`.
   Future<void> _fetchContact(String number) async {
-    try {
-      final contact = await _contactsRepository.getContactByPhoneNumber(number);
-      if (isClosed) return;
-      emit(state.copyWith(contact: contact));
-    } catch (_) {
-      if (isClosed) return;
-      emit(state.copyWith(contact: null));
-    }
+    final contact = await _contactsRepository.getContactByPhoneNumber(number);
+    if (isClosed) return;
+
+    emit(state.copyWith(contact: contact));
+  }
+
+  @override
+  Future<void> close() {
+    _contactDebounce.dispose();
+    return super.close();
   }
 }
