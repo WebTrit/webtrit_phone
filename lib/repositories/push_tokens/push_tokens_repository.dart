6e19399ea5ff4d3f1f52801c7ee39f5eff96ff77
Vladislav Komelkov <v.komelkov@webtrit.com>
2026-08-06 16:14:35 +0300

    feat: ios push mode specify (#1553)

--- lib/repositories/push_tokens/push_tokens_repository.dart
+++ lib/repositories/push_tokens/push_tokens_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import 'package:webtrit_api/webtrit_api.dart';
 
 import 'package:webtrit_phone/app/session/session.dart';
@@ -15,7 +17,19 @@ class PushTokensRepository {
   final SessionGuard _sessionGuard;
 
   Future<void> insertOrUpdatePushToken(AppPushTokenType type, String value) async {
-    var appPushToken = AppPushToken(type: type, value: value);
+    // If you launch the app in debug mode, then you definetely want to use dev environment for push tokens
+    // Alternatively you can use ios entitlements from native part, but use kDebugMode is just simpler and cover 99% dev cases
+    //
+    // fcm on ios handles dev/prod automatically, so we don't need to worry about that
+    final env = switch ((type, kDebugMode)) {
+      (AppPushTokenType.apkvoip, true) => AppPushTokenEnv.dev,
+      (AppPushTokenType.apkvoip, false) => AppPushTokenEnv.prod,
+      (AppPushTokenType.apns, true) => AppPushTokenEnv.dev,
+      (AppPushTokenType.apns, false) => AppPushTokenEnv.prod,
+      _ => null,
+    };
+
+    var appPushToken = AppPushToken(type: type, value: value, env: env);
 
     try {
       return await _webtritApiClient.createAppPushToken(_token, appPushToken);
