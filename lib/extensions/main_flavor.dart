d39e8aae298d61a77a2cd8c8b655de2567d048e7
Dmitro Serdun <d.serdun@webtrit.com>
2026-08-19 22:20:20 +0300

    fix: recognise the call history as the recents tab (WT-1745) (#1695)

--- lib/extensions/main_flavor.dart
+++ lib/extensions/main_flavor.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/foundation.dart';
 
 import 'package:webtrit_phone/app/keys.dart';
+import 'package:webtrit_phone/app/router/app_router.dart';
 import 'package:webtrit_phone/models/main_flavor.dart';
+
+/// Which section of the main screen a tab route belongs to, or `null` when the
+/// route is not one of its tabs.
+///
+/// A section can be reached by more than one route: the recents tab opens the
+/// call history when the backend can answer for it and the plain list
+/// otherwise, and both are the same tab to everything outside the router.
+MainFlavor? mainFlavorOfTabRoute(String routeName) {
+  switch (routeName) {
+    case FavoritesRouterPageRoute.name:
+      return MainFlavor.favorites;
+    case RecentsRouterPageRoute.name:
+    case RecentCdrsRouterPageRoute.name:
+      return MainFlavor.recents;
+    case ContactsRouterPageRoute.name:
+      return MainFlavor.contacts;
+    case KeypadScreenPageRoute.name:
+      return MainFlavor.keypad;
+    case ConversationsScreenPageRoute.name:
+      return MainFlavor.messaging;
+    case EmbeddedTabPageRoute.name:
+      return MainFlavor.embedded;
+    default:
+      return null;
+  }
+}
 
 extension LoginTypeLoginSegmentKey on MainFlavor {
   /// Widget key of the bottom navigation entry of a KNOWN fixed section -
--- lib/features/main/widgets/main_screen_route_observer.dart
+++ lib/features/main/widgets/main_screen_route_observer.dart
@@ -1,8 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
-import 'package:webtrit_phone/app/router/app_router.dart';
-import 'package:webtrit_phone/models/main_flavor.dart';
+import 'package:webtrit_phone/extensions/extensions.dart';
 
 import 'package:webtrit_phone/repositories/repositories.dart';
 
@@ -16,14 +15,14 @@ class MainScreenNavigatorObserver extends AutoRouterObserver {
   @override
   void didInitTabRoute(TabPageRoute route, TabPageRoute? previousRoute) {
     _logger.finest('didInitTabRoute: ${route.routeInfo.name}');
-    _mainScreenRouteStateRepository.setActiveTab(_mapActiveTabPageToFlavor(route.routeInfo.name));
+    _mainScreenRouteStateRepository.setActiveTab(mainFlavorOfTabRoute(route.routeInfo.name));
     super.didInitTabRoute(route, previousRoute);
   }
 
   @override
   void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
     _logger.finest('didChangeTabRoute: ${route.routeInfo.name}');
-    _mainScreenRouteStateRepository.setActiveTab(_mapActiveTabPageToFlavor(route.routeInfo.name));
+    _mainScreenRouteStateRepository.setActiveTab(mainFlavorOfTabRoute(route.routeInfo.name));
     super.didChangeTabRoute(route, previousRoute);
   }
 
@@ -37,24 +36,5 @@ class MainScreenNavigatorObserver extends AutoRouterObserver {
   void didPop(Route route, Route? previousRoute) {
     _logger.finest('didPop: ${route.settings.name}');
     super.didPop(route, previousRoute);
-  }
-
-  MainFlavor? _mapActiveTabPageToFlavor(String activeTabPage) {
-    switch (activeTabPage) {
-      case FavoritesRouterPageRoute.name:
-        return MainFlavor.favorites;
-      case RecentsRouterPageRoute.name:
-        return MainFlavor.recents;
-      case ContactsRouterPageRoute.name:
-        return MainFlavor.contacts;
-      case KeypadScreenPageRoute.name:
-        return MainFlavor.keypad;
-      case ConversationsScreenPageRoute.name:
-        return MainFlavor.messaging;
-      case EmbeddedTabPageRoute.name:
-        return MainFlavor.embedded;
-      default:
-        return null;
-    }
   }
 }
